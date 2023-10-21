340808ea67ac505a0bca2a4d4bbc010f920042cd
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-21 16:51:17 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/programmers/bucketback/BucketBackApplication.java
@@ -0,0 +1,13 @@
+package com.programmers.bucketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketBackApplication.class, args);
+	}
+
+}
