b2b02494eab1121064807206b757e72ed829bb08
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-26 00:19:00 +0900

    feat: 시큐리티 설정 변경

--- src/main/java/com/programmers/bucketback/global/config/security/SecurityConfiguration.java
+++ src/main/java/com/programmers/bucketback/global/config/security/SecurityConfiguration.java
@@ -1,18 +1,16 @@
 package com.programmers.bucketback.global.config.security;
 
+import com.programmers.bucketback.global.config.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import com.programmers.bucketback.global.config.security.jwt.JwtAuthenticationFilter;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +23,7 @@ public class SecurityConfiguration {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable()
-			)
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(authorize -> authorize
 				.requestMatchers(new AntPathRequestMatcher("/api/v1/auth/*")).permitAll()
 				.anyRequest().authenticated()
