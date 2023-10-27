9d0a735b89d6e3a89c1fa18bcf4c2c8d83a38b9e
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-27 17:29:16 +0900

    feat: SecurityContextHolder에서 현재 회원Id 가져오기

--- src/main/java/com/programmers/bucketback/global/error/exception/ErrorCode.java
+++ src/main/java/com/programmers/bucketback/global/error/exception/ErrorCode.java
@@ -10,10 +10,12 @@ public enum ErrorCode {
 	// Common
 	INTERNAL_SERVER_ERROR("COMMON_001", "Internal Server Error"),
 	INVALID_REQUEST("COMMON_002", "유효하지 않은 요청입니다."),
+	SECURITY_CONTEXT_NOT_FOUND("COMMON_003", "Security context를 찾을 수 없습니다."),
 
 	// Member
 	MEMBER_LOGIN_FAIL("MEMBER_001", "로그인 정보가 잘못 되었습니다."),
 	MEMBER_NOT_FOUND("MEMBER_002", "회원을 찾을 수 없습니다."),
+	MEMBER_ANONYMOUS("MEMBER_003", "익명의 사용자 입니다.")
 	;
 
 	private final String code;
