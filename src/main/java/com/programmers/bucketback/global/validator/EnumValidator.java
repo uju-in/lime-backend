d5c86b8388ba4464b1b5d9fd7d90920d642e8b1c
Curry4182 <kangbk4182@naver.com>
2023-10-27 10:44:48 +0900

    feat: 입력받은 데이터 검증하는 enum annotation 추가

---
+++ src/main/java/com/programmers/bucketback/global/validator/EnumValidator.java
@@ -0,0 +1,22 @@
+package com.programmers.bucketback.global.validator;
+
+import java.util.Arrays;
+
+import com.programmers.bucketback.global.annotation.Enum;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<Enum, java.lang.Enum> {
+	@Override
+	public boolean isValid(
+		java.lang.Enum value,
+		ConstraintValidatorContext context
+	) {
+		if (value == null)
+			return false;
+		Class<?> reflectionEnumClass = value.getDeclaringClass();
+
+		return Arrays.asList(reflectionEnumClass.getEnumConstants()).contains(value);
+	}
+}
