99427d9ab1202b2d02c3ecca864ce33c06383583
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-28 21:11:14 +0900

    !BREAKING CHANGE: 로그인 시 memberId 반환

--- src/main/java/com/programmers/bucketback/domains/member/api/dto/response/MemberLoginResponse.java
+++ src/main/java/com/programmers/bucketback/domains/member/api/dto/response/MemberLoginResponse.java
@@ -1,7 +1,7 @@
 package com.programmers.bucketback.domains.member.api.dto.response;
 
 public record MemberLoginResponse(
-        String nickname,
+        Long memberId,
         String jwtToken
 ) {
 }
--- src/main/java/com/programmers/bucketback/domains/member/application/dto/response/LoginMemberServiceResponse.java
+++ src/main/java/com/programmers/bucketback/domains/member/application/dto/response/LoginMemberServiceResponse.java
@@ -3,10 +3,10 @@ package com.programmers.bucketback.domains.member.application.dto.response;
 import com.programmers.bucketback.domains.member.api.dto.response.MemberLoginResponse;
 
 public record LoginMemberServiceResponse(
-	String nickname,
+	Long memberId,
 	String jwtToken
 ) {
 	public MemberLoginResponse toMemberLoginResponse() {
-		return new MemberLoginResponse(nickname, jwtToken);
+		return new MemberLoginResponse(memberId, jwtToken);
 	}
 }
