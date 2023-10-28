03fa42f65fb098d38bbf6e291b5917944c61e2a3
Yiseul Park <omjl5123@catholic.ac.kr>
2023-10-28 21:05:05 +0900

    feat: 서비스에 트랜잭션 읽기 모드 추가

--- src/main/java/com/programmers/bucketback/domains/member/application/MemberService.java
+++ src/main/java/com/programmers/bucketback/domains/member/application/MemberService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
 
 	private final SecurityManager securityManager;
