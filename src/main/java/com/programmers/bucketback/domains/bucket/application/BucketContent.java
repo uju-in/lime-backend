7025ce47a22643747582821979610d041ed50aa6
JoJaeHyeon <joje1003@naver.com>
2023-10-29 18:21:51 +0900

    feat : bucket 상세 조회 구현 및 컨벤션 정리

--- src/main/java/com/programmers/bucketback/domains/bucket/application/BucketContent.java
+++ src/main/java/com/programmers/bucketback/domains/bucket/application/BucketContent.java
@@ -2,8 +2,8 @@ package com.programmers.bucketback.domains.bucket.application;
 
 import java.util.List;
 
+import com.programmers.bucketback.domains.bucket.domain.Bucket;
 import com.programmers.bucketback.domains.common.Hobby;
-
 import lombok.Builder;
 
 @Builder
@@ -12,7 +12,17 @@ public record BucketContent(
 	Hobby hobby,
 	String name,
 	Integer budget,
-	List<Long> itemIds
+	List<Long> itemIds,
+	Long memberId,
+	Long bucketId
 ) {
-
+	public static BucketContent from(final Bucket bucket){
+		return BucketContent.builder()
+			.hobby(bucket.getHobby())
+			.name(bucket.getBucketInfo().getName())
+			.budget(bucket.getBucketInfo().getBudget())
+			.bucketId(bucket.getId())
+			.memberId(bucket.getMemberId())
+			.build();
+	}
 }
