cb4f11f17e4cf8899f6eb190e90504784a990011
Curry4182 <kangbk4182@naver.com>
2023-10-29 23:06:18 +0900

    refactor: 서비스에 필요없는 검증 로직 삭제

--- src/main/java/com/programmers/bucketback/domains/item/application/dto/AddMemberItemServiceRequest.java
+++ src/main/java/com/programmers/bucketback/domains/item/application/dto/AddMemberItemServiceRequest.java
@@ -2,13 +2,7 @@ package com.programmers.bucketback.domains.item.application.dto;
 
 import java.util.List;
 
-import jakarta.validation.constraints.NotNull;
-
 public record AddMemberItemServiceRequest(
-	@NotNull
 	List<Long> itemIds
 ) {
-	public AddMemberItemServiceRequest(@NotNull final List<Long> itemIds) {
-		this.itemIds = itemIds;
-	}
 }
--- src/main/java/com/programmers/bucketback/domains/item/application/dto/CreateItemServiceRequest.java
+++ src/main/java/com/programmers/bucketback/domains/item/application/dto/CreateItemServiceRequest.java
@@ -3,24 +3,14 @@ package com.programmers.bucketback.domains.item.application.dto;
 import com.programmers.bucketback.domains.common.Hobby;
 import com.programmers.bucketback.domains.item.application.crawling.ItemInfo;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CreateItemServiceRequest(
-	@NotNull
 	Hobby hobby,
-
-	@NotNull
 	String itemName,
-
-	@NotNull
 	Integer price,
-
-	@NotNull
 	String imageUrl,
-
-	@NotNull
 	String url
 ) {
 
