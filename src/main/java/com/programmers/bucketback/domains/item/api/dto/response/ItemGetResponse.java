e40e2ed051a77116260278a630e53eee412fc035
Curry4182 <kangbk4182@naver.com>
2023-11-02 00:57:14 +0900

    feat: 아이템 상세조회 기능 추가

---
+++ src/main/java/com/programmers/bucketback/domains/item/api/dto/response/ItemGetResponse.java
@@ -0,0 +1,15 @@
+package com.programmers.bucketback.domains.item.api.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public record ItemGetResponse(
+	Long itemId,
+	String itemImage,
+	String itemName,
+	Integer itemPrice,
+	String itemUrl,
+	Double itemAvgRate,
+	boolean itemIsMine
+) {
+}
--- src/main/java/com/programmers/bucketback/domains/item/application/ItemService.java
+++ src/main/java/com/programmers/bucketback/domains/item/application/ItemService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.programmers.bucketback.domains.common.MemberUtils;
 import com.programmers.bucketback.domains.item.application.dto.AddMemberItemServiceRequest;
+import com.programmers.bucketback.domains.item.application.dto.GetItemServiceResponse;
+import com.programmers.bucketback.domains.item.domain.Item;
+import com.programmers.bucketback.domains.review.application.ReviewStatistics;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +15,33 @@ import lombok.RequiredArgsConstructor;
 public class ItemService {
 
 	private final AddMemberItemService addMemberItemService;
+	private final MemberItemChecker memberItemChecker;
+	private final ItemReader itemReader;
+	private final ReviewStatistics reviewStatistics;
 
 	public void addItem(final AddMemberItemServiceRequest request) {
 		Long memberId = MemberUtils.getCurrentMemberId();
 		addMemberItemService.addMemberItems(request.itemIds(), memberId);
+	}
+
+	public GetItemServiceResponse getItemDetails(Long itemId) {
+		boolean itemIsMine = false;
+
+		Item item = itemReader.read(itemId);
+		if (MemberUtils.isLoggedIn()) {
+			Long memberId = MemberUtils.getCurrentMemberId();
+			itemIsMine = memberItemChecker.existMemberItemByMemberId(memberId, item);
+		}
+
+		Double itemAvgRating = reviewStatistics.getReviewAvgByItemId(itemId);
+		return GetItemServiceResponse.builder()
+			.itemId(item.getId())
+			.itemIsMine(itemIsMine)
+			.itemUrl(item.getUrl())
+			.itemAvgRate(itemAvgRating)
+			.itemPrice(item.getPrice())
+			.itemName(item.getName())
+			.itemImage(item.getImage())
+			.build();
 	}
 }
