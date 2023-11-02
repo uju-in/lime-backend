c7761634066ca0739c368d1febfdd2a91154287b
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-02 12:56:41 +0900

    refactor: 매개변수 4개 이상 빌더 사용

--- src/main/java/com/programmers/bucketback/domains/vote/application/OptionItem.java
+++ src/main/java/com/programmers/bucketback/domains/vote/application/OptionItem.java
@@ -2,6 +2,9 @@ package com.programmers.bucketback.domains.vote.application;
 
 import com.programmers.bucketback.domains.item.domain.Item;
 
+import lombok.Builder;
+
+@Builder
 public record OptionItem(
 	Long id,
 	String name,
@@ -9,11 +12,11 @@ public record OptionItem(
 	String image
 ) {
 	public static OptionItem from(final Item item) {
-		return new OptionItem(
-			item.getId(),
-			item.getName(),
-			item.getPrice(),
-			item.getImage()
-		);
+		return OptionItem.builder()
+			.id(item.getId())
+			.name(item.getName())
+			.price(item.getPrice())
+			.image(item.getImage())
+			.build();
 	}
 }
