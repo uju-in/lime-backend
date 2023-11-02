2826c3ee6a10b5d912b1272a5724b42b0dd45e1e
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-02 21:57:20 +0900

    feet: isOwner 추가, 투표 안했을 시 조회 안되는 버그 해결

--- src/main/java/com/programmers/bucketback/domains/vote/api/dto/response/VoteGetResponse.java
+++ src/main/java/com/programmers/bucketback/domains/vote/api/dto/response/VoteGetResponse.java
@@ -1,9 +1,8 @@
 package com.programmers.bucketback.domains.vote.api.dto.response;
 
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.programmers.bucketback.domains.vote.application.OptionItem;
+import com.programmers.bucketback.domains.vote.application.VoteInfo;
 import com.programmers.bucketback.domains.vote.application.dto.response.GetVoteServiceResponse;
 
 import lombok.Builder;
@@ -12,12 +11,8 @@ import lombok.Builder;
 public record VoteGetResponse(
 	OptionItem option1Item,
 	OptionItem option2Item,
-	String content,
-	LocalDateTime createAt,
-	boolean isVoting,
-	int option1Votes,
-	int option2Votes,
-	int participants,
+	VoteInfo voteInfo,
+	boolean isOwner,
 
 	@JsonInclude(JsonInclude.Include.NON_NULL)
 	Long selectedItemId
@@ -26,12 +21,8 @@ public record VoteGetResponse(
 		return VoteGetResponse.builder()
 			.option1Item(voteServiceResponse.option1Item())
 			.option2Item(voteServiceResponse.option2Item())
-			.content(voteServiceResponse.content())
-			.createAt(voteServiceResponse.createAt())
-			.isVoting(voteServiceResponse.isVoting())
-			.option1Votes(voteServiceResponse.option1Votes())
-			.option2Votes(voteServiceResponse.option2Votes())
-			.participants(voteServiceResponse.participants())
+			.voteInfo(voteServiceResponse.voteInfo())
+			.isOwner(voteServiceResponse.isOwner())
 			.selectedItemId(voteServiceResponse.selectedItemId())
 			.build();
 	}
