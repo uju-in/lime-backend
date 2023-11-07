6a1b7ef0f253dca51639f0ba325203b1bca613d8
JoJaeHyeon <joje1003@naver.com>
2023-11-07 18:49:39 +0900

    refactor : 연관관계 joinColumn 사용

--- src/main/java/com/programmers/bucketback/domains/feed/domain/FeedLike.java
+++ src/main/java/com/programmers/bucketback/domains/feed/domain/FeedLike.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -28,7 +29,7 @@ public class FeedLike {
 	private Long memberId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name = "feed_id")
+	@JoinColumn(name = "feed_id")
 	private Feed feed;
 
 	@Builder
