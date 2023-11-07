7ef383051599dc9e175a5c4b1a2afe7cfd9998e1
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-07 23:53:58 +0900

    fix: @PostMapping 추가

--- src/main/java/com/programmers/bucketback/domains/comment/api/CommentController.java
+++ src/main/java/com/programmers/bucketback/domains/comment/api/CommentController.java
@@ -2,6 +2,7 @@ package com.programmers.bucketback.domains.comment.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class CommentController {
 
 	private final CommentService commentService;
 
+	@PostMapping
 	public ResponseEntity<Void> createComment(
 		@PathVariable final Long feedId,
 		@Valid @RequestBody final CommentCreateRequest request
