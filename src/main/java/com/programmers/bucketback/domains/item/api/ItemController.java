ac2e956ec8d2053460e3bd9db770071d8ded7feb
Curry4182 <kangbk4182@naver.com>
2023-11-07 19:16:49 +0900

    fix: 잘못된 path 수정

--- src/main/java/com/programmers/bucketback/domains/item/api/ItemController.java
+++ src/main/java/com/programmers/bucketback/domains/item/api/ItemController.java
@@ -42,7 +42,7 @@ public class ItemController {
 		return ResponseEntity.ok().build();
 	}
 
-	@PostMapping("/myItem")
+	@PostMapping("/myItems")
 	public ResponseEntity<Void> addItems(@Valid @RequestBody final MemberItemAddRequest request) {
 		itemService.addItem(request.toAddMemberItemServiceRequest());
 
