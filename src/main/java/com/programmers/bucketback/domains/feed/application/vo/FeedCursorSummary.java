ee40592285bb6acb21b973ee102c6774925934a1
Curry4182 <kangbk4182@naver.com>
2023-11-13 18:18:36 +0900

    feat: 피드 목록 조회를 위한 dto 추가

---
+++ src/main/java/com/programmers/bucketback/domains/feed/application/vo/FeedCursorSummary.java
@@ -0,0 +1,33 @@
+package com.programmers.bucketback.domains.feed.application.vo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.programmers.bucketback.domains.member.application.vo.MemberInfo;
+
+import lombok.Builder;
+
+@Builder
+public record FeedCursorSummary(
+	String cursorId,
+	MemberInfo memberInfo,
+	Long feedId,
+	String feedContent,
+	int likeCount,
+	int commentCount,
+	LocalDateTime createdAt,
+	List<FeedCursorItem> feedItems
+) {
+	public FeedCursorSummaryLike of(final boolean isLike) {
+		return FeedCursorSummaryLike.builder()
+			.cursorId(cursorId)
+			.memberInfo(memberInfo)
+			.feedId(feedId)
+			.likeCount(likeCount)
+			.commentCount(commentCount)
+			.createdAt(createdAt)
+			.feedItems(feedItems)
+			.isLike(isLike)
+			.build();
+	}
+}
