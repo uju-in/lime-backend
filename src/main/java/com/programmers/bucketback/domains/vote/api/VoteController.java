3335f1fb27a79bd08f8b109a9a1c1ec314c4c5aa
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-15 02:29:54 +0900

    feat: vote swagger 추가

--- src/main/java/com/programmers/bucketback/domains/vote/api/VoteController.java
+++ src/main/java/com/programmers/bucketback/domains/vote/api/VoteController.java
@@ -1,16 +1,5 @@
 package com.programmers.bucketback.domains.vote.api;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.programmers.bucketback.domains.common.Hobby;
 import com.programmers.bucketback.domains.common.vo.CursorRequest;
 import com.programmers.bucketback.domains.vote.api.dto.request.VoteCreateRequest;
@@ -23,10 +12,14 @@ import com.programmers.bucketback.domains.vote.application.dto.request.VoteSortC
 import com.programmers.bucketback.domains.vote.application.dto.request.VoteStatusCondition;
 import com.programmers.bucketback.domains.vote.application.dto.response.GetVoteServiceResponse;
 import com.programmers.bucketback.domains.vote.application.dto.response.GetVotesServiceResponse;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "votes", description = "투표 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/votes")
@@ -34,6 +27,7 @@ public class VoteController {
 
 	private final VoteService voteService;
 
+	@Operation(summary = "투표 생성", description = "VoteCreateRequest 을 이용하여 투표를 생성힙니다.")
 	@PostMapping
 	public ResponseEntity<VoteCreateResponse> createVote(@Valid @RequestBody final VoteCreateRequest request) {
 		final Long voteId = voteService.createVote(request.toCreateVoteServiceRequest());
@@ -42,6 +36,7 @@ public class VoteController {
 		return ResponseEntity.ok(response);
 	}
 
+	@Operation(summary = "투표 참여", description = "VoteId, VoteParticipateRequest 을 이용하여 투표에 참여힙니다.")
 	@PostMapping("/{voteId}/participation")
 	public ResponseEntity<Void> participateVote(
 		@PathVariable final Long voteId,
@@ -52,6 +47,7 @@ public class VoteController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "투표 참여 취소", description = "VoteId를 이용하여 투표 참여를 취소합니다.")
 	@DeleteMapping("/{voteId}/cancel")
 	public ResponseEntity<Void> cancelVote(@PathVariable final Long voteId) {
 		voteService.cancelVote(voteId);
@@ -59,6 +55,7 @@ public class VoteController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "투표 삭제", description = "VoteId를 이용하여 투표를 삭제합니다.")
 	@DeleteMapping("/{voteId}")
 	public ResponseEntity<Void> deleteVote(@PathVariable final Long voteId) {
 		voteService.deleteVote(voteId);
@@ -66,6 +63,7 @@ public class VoteController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "투표 상세 조회", description = "VoteId를 이용하여 투표를 조회합니다.")
 	@GetMapping("/{voteId}")
 	public ResponseEntity<VoteGetResponse> getVote(@PathVariable final Long voteId) {
 		final GetVoteServiceResponse serviceResponse = voteService.getVote(voteId);
@@ -74,6 +72,7 @@ public class VoteController {
 		return ResponseEntity.ok(response);
 	}
 
+	@Operation(summary = "투표 목록 조회(커서)", description = "취미, 상태 조건, 정렬 조건, 커서 요청 정보를 이용하여 투표를 조회합니다.")
 	@GetMapping
 	public ResponseEntity<VoteGetByCursorResponse> getVotesByCursor(
 		@RequestParam final String hobby,
--- src/main/java/com/programmers/bucketback/domains/vote/api/dto/request/VoteParticipateRequest.java
+++ src/main/java/com/programmers/bucketback/domains/vote/api/dto/request/VoteParticipateRequest.java
@@ -1,8 +1,10 @@
 package com.programmers.bucketback.domains.vote.api.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record VoteParticipateRequest(
+	@Schema(description = "아이템 ID", example = "1")
 	@NotNull(message = "아이템 ID는 필수 값입니다.")
 	Long itemId
 ) {
