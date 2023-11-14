626c68e23e5302e6ad4bb0bde0790d0fd68a236b
JoJaeHyeon <joje1003@naver.com>
2023-11-14 15:05:29 +0900

    chore : 메소드 네이밍 변경

--- src/main/java/com/programmers/bucketback/domains/bucket/application/vo/BucketGetServiceResponse.java
+++ src/main/java/com/programmers/bucketback/domains/bucket/application/vo/BucketGetServiceResponse.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.programmers.bucketback.domains.bucket.domain.Bucket;
 import com.programmers.bucketback.domains.item.application.vo.ItemInfo;
 
-public record GetBucketServiceResponse(
+public record BucketGetServiceResponse(
 	Bucket bucket,
 	List<ItemInfo> itemInfos
 ) {
