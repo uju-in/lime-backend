c7bc86bae263db88653bbddcda052f4fe8381155
JoJaeHyeon <joje1003@naver.com>
2023-11-14 11:58:15 +0900

    refactor : 응답 형식 변경

--- src/main/java/com/programmers/bucketback/domains/member/application/vo/MemberProfile.java
+++ src/main/java/com/programmers/bucketback/domains/member/application/vo/MemberProfile.java
@@ -2,17 +2,23 @@ package com.programmers.bucketback.domains.member.application.vo;
 
 import com.programmers.bucketback.domains.member.domain.Member;
 
+import lombok.Builder;
+
+@Builder
 public record MemberProfile(
-	MemberInfo memberInfo,
+	Long memberId,
+	String nickname,
+	// String profileImage, //refactor : 도메인 필드 추가시 반영
+	Integer levelPoint,
 	String introduction
 ) {
 	public static MemberProfile from(final Member member) {
-		MemberInfo memberInfo = MemberInfo.builder()
+		return MemberProfile.builder()
 			.memberId(member.getId())
-			.nickName(member.getNickname())
-			.level(member.getLevelPoint())
+			.nickname(member.getNickname())
+			// .profileImage(member.getProfileImage())
+			.levelPoint(member.getLevelPoint())
+			.introduction(member.getIntroduction())
 			.build();
-
-		return new MemberProfile(memberInfo, member.getIntroduction());
 	}
 }
