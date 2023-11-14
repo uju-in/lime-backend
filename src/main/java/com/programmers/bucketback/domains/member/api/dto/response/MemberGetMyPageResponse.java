1c8e8da38d9cfb232cbb84fcc4ea796fb0c9b800
JoJaeHyeon <joje1003@naver.com>
2023-11-14 11:44:17 +0900

    refactor : 응답 형식 수정

--- src/main/java/com/programmers/bucketback/domains/member/api/dto/response/MemberGetMyPageResponse.java
+++ src/main/java/com/programmers/bucketback/domains/member/api/dto/response/MemberGetMyPageResponse.java
@@ -1,11 +1,22 @@
 package com.programmers.bucketback.domains.member.api.dto.response;
 
+import java.util.List;
+
+import com.programmers.bucketback.domains.member.application.InventoryProfile;
+import com.programmers.bucketback.domains.member.application.vo.BucketProfile;
+import com.programmers.bucketback.domains.member.application.vo.MemberProfile;
 import com.programmers.bucketback.domains.member.application.vo.MyPage;
 
 public record MemberGetMyPageResponse(
-	MyPage myPage
+	MemberProfile memberProfile,
+	List<BucketProfile> bucketProfiles,
+	List<InventoryProfile> inventoryProfiles
 ) {
 	public static MemberGetMyPageResponse from(final MyPage myPage) {
-		return new MemberGetMyPageResponse(myPage);
+		return new MemberGetMyPageResponse(
+			myPage.memberProfile(),
+			myPage.bucketProfiles(),
+			myPage.inventoryProfiles()
+		);
 	}
 }
