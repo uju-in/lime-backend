7587e4a1f9e8ea32bdad63d3e1c454a5c01ee9a3
Curry4182 <kangbk4182@naver.com>
2023-11-15 04:41:57 +0900

    feat: 사용자가 담은 아이템이 있는지 확인하는 JPQL 메서드 추가

--- src/main/java/com/programmers/bucketback/domains/item/repository/MemberItemRepository.java
+++ src/main/java/com/programmers/bucketback/domains/item/repository/MemberItemRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.programmers.bucketback.domains.item.domain.Item;
 import com.programmers.bucketback.domains.item.domain.MemberItem;
