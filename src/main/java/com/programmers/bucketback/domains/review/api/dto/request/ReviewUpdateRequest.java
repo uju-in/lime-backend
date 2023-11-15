9afb02b7cb5a9608b8cae3d943dde31507c4f7c2
Curry4182 <kangbk4182@naver.com>
2023-11-16 03:45:11 +0900

    feat: 리뷰 dto에 대한 swagger schema 추가

--- src/main/java/com/programmers/bucketback/domains/review/api/dto/request/ReviewUpdateRequest.java
+++ src/main/java/com/programmers/bucketback/domains/review/api/dto/request/ReviewUpdateRequest.java
@@ -2,17 +2,20 @@ package com.programmers.bucketback.domains.review.api.dto.request;
 
 import com.programmers.bucketback.domains.review.application.vo.ReviewContent;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record ReviewUpdateRequest(
 
+	@Schema(description = "리뷰 평점", example = "3")
 	@NotNull(message = "리뷰 평점은 필수 값입니다.")
 	@Min(value = 1, message = "리뷰 평점은 최소 1점 입니다.")
 	@Max(value = 5, message = "리뷰 평점은 최대 5점 입니다.")
 	Integer rating,
 
+	@Schema(description = "리뷰 내용", example = "사실 제가 원하는 않은 스타일이에요")
 	String content
 ) {
 
