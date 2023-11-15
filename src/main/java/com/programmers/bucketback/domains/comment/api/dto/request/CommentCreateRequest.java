110f12a8e4ddce9b6472ed1b13f74cee9a5be616
Curry4182 <kangbk4182@naver.com>
2023-11-15 16:50:01 +0900

    fix: 댓글 생성 메시지 수정

--- src/main/java/com/programmers/bucketback/domains/comment/api/dto/request/CommentCreateRequest.java
+++ src/main/java/com/programmers/bucketback/domains/comment/api/dto/request/CommentCreateRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record CommentCreateRequest(
-		@Size(max = 1000, message = "댓글 내용은 최대 300자 입니다.")
-		@NotNull(message = "댓글 내용은 필수 값입니다.")
-		String content
+	@Size(max = 300, message = "댓글 내용은 최대 300자 입니다.")
+	@NotNull(message = "댓글 내용은 필수 값입니다.")
+	String content
 ) {
 }
