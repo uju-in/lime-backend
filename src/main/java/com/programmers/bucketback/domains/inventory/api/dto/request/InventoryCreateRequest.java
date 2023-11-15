559b3a35691840eb33af6d6461743910db7182a4
JoJaeHyeon <joje1003@naver.com>
2023-11-15 11:50:45 +0900

    refactor : 검증 로직 보완

--- src/main/java/com/programmers/bucketback/domains/inventory/api/dto/request/InventoryCreateRequest.java
+++ src/main/java/com/programmers/bucketback/domains/inventory/api/dto/request/InventoryCreateRequest.java
@@ -7,7 +7,7 @@ import com.programmers.bucketback.domains.common.Hobby;
 import com.programmers.bucketback.global.annotation.Enum;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
 public record InventoryCreateRequest(
 
@@ -16,8 +16,9 @@ public record InventoryCreateRequest(
 	Hobby hobby,
 
 	@Schema(description = "아이템 아이디 값", example = "[1,2,3]")
-	@NotNull
+	@NotEmpty(message = "아이템 id를 최소 1개 이상 입력하세요")
 	List<Long> itemIds
+
 ) {
 	public ItemIdRegistry toRegistry() {
 		return new ItemIdRegistry(itemIds);
--- src/main/java/com/programmers/bucketback/domains/inventory/api/dto/request/InventoryUpdateRequest.java
+++ src/main/java/com/programmers/bucketback/domains/inventory/api/dto/request/InventoryUpdateRequest.java
@@ -5,12 +5,12 @@ import java.util.List;
 import com.programmers.bucketback.domains.bucket.application.vo.ItemIdRegistry;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 
 public record InventoryUpdateRequest(
 
 	@Schema(description = "아이템 아이디 값", example = "[1,2,3]")
-	@NotNull
+	@NotEmpty(message = "아이템 id를 최소 1개 이상 입력하세요")
 	List<Long> itemIds
 ) {
 	public ItemIdRegistry toRegistry() {
