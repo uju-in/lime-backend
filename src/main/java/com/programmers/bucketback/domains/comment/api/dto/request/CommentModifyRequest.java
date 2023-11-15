932e6404fe4313e3732bdf9593d12b7fad6e7ab3
Curry4182 <kangbk4182@naver.com>
2023-11-15 16:50:34 +0900

    feat: 댓글 수정 검증 메시지 추가

--- src/main/java/com/programmers/bucketback/domains/comment/api/dto/request/CommentModifyRequest.java
+++ src/main/java/com/programmers/bucketback/domains/comment/api/dto/request/CommentModifyRequest.java
@@ -1,9 +1,11 @@
 package com.programmers.bucketback.domains.comment.api.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CommentModifyRequest(
-	@NotNull
+	@Size(max = 300, message = "댓글 내용은 최대 300자 입니다.")
+	@NotNull(message = "댓글 내용은 필수 값입니다.")
 	String content
 ) {
 }
