1435ebe28ef244f8476b239d8dcd2e19f10aac60
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-16 14:31:02 +0900

    rename: aop 클래스명 수정

--- src/main/java/com/programmers/bucketback/global/level/PointManagerAop.java
+++ src/main/java/com/programmers/bucketback/global/level/PointManagerAop.java
@@ -1,21 +1,23 @@
 package com.programmers.bucketback.global.level;
 
-import com.programmers.bucketback.domains.member.application.MemberReader;
-import com.programmers.bucketback.domains.member.domain.Member;
-import com.programmers.bucketback.domains.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import java.lang.reflect.Method;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.Method;
+import com.programmers.bucketback.domains.member.application.MemberReader;
+import com.programmers.bucketback.domains.member.domain.Member;
+import com.programmers.bucketback.domains.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
-public class PointManager {
+public class PointManagerAop {
 
     private final MemberReader memberReader;
     private final MemberRepository memberRepository;
