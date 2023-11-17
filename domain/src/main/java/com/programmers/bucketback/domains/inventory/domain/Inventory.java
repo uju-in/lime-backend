e1f357291574d1f74536b5abf2a6eb0f4f7503d6
JoJaeHyeon <joje1003@naver.com>
2023-11-17 17:01:24 +0900

    refactor : notnull 의존성 삭제로 인한 검증 방식 변경

--- domain/src/main/java/com/programmers/bucketback/domains/inventory/domain/Inventory.java
+++ domain/src/main/java/com/programmers/bucketback/domains/inventory/domain/Inventory.java
@@ -2,6 +2,7 @@ package com.programmers.bucketback.domains.inventory.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import com.programmers.bucketback.Hobby;
 import com.programmers.bucketback.domains.BaseEntity;
@@ -16,7 +17,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,29 +27,24 @@ import lombok.NoArgsConstructor;
 @Table(name = "inventories")
 public class Inventory extends BaseEntity {
 
+	@OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL)
+	private final List<InventoryItem> inventoryItems = new ArrayList<>();
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
-
-	@NotNull
 	@Column(name = "member_id")
 	private Long memberId;
-
-	@NotNull
 	@Column(name = "hobby")
 	@Enumerated(EnumType.STRING)
 	private Hobby hobby;
 
-	@OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL)
-	private final List<InventoryItem> inventoryItems = new ArrayList<>();
-
 	public Inventory(
-		@NotNull final Long memberId,
-		@NotNull final Hobby hobby
+		final Long memberId,
+		final Hobby hobby
 	) {
-		this.memberId = memberId;
-		this.hobby = hobby;
+		this.memberId = Objects.requireNonNull(memberId);
+		this.hobby = Objects.requireNonNull(hobby);
 	}
 
 	public void addInventoryItem(final InventoryItem inventoryItem) {
