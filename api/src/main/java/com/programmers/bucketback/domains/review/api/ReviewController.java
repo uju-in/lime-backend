a73e67044ca258598cf9c3d6a8a93c5bfcb03c1a
Curry4182 <kangbk4182@naver.com>
2023-11-17 17:58:14 +0900

    refactor: 멀티 모듈 적용을 위한 리뷰 리팩토링

--- api/src/main/java/com/programmers/bucketback/domains/review/api/ReviewController.java
+++ api/src/main/java/com/programmers/bucketback/domains/review/api/ReviewController.java
@@ -1,17 +1,27 @@
 package com.programmers.bucketback.domains.review.api;
 
-import com.programmers.bucketback.domains.common.vo.CursorRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programmers.bucketback.common.cursor.CursorRequest;
 import com.programmers.bucketback.domains.review.api.dto.request.ReviewCreateRequest;
 import com.programmers.bucketback.domains.review.api.dto.request.ReviewUpdateRequest;
 import com.programmers.bucketback.domains.review.api.dto.response.ReviewGetByCursorResponse;
 import com.programmers.bucketback.domains.review.application.ReviewService;
 import com.programmers.bucketback.domains.review.application.dto.ReviewGetByCursorServiceResponse;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "review", description = "리뷰 API")
 @RestController
