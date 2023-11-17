dd7189e4b3ee808570fc838b0b50048dbf89919a
Curry4182 <kangbk4182@naver.com>
2023-11-17 22:37:18 +0900

    refactor: 멀티모듈을 적용하기 위한 피드목록조회 리팩토링 진행

--- api/src/main/java/com/programmers/bucketback/domains/feed/application/FeedService.java
+++ api/src/main/java/com/programmers/bucketback/domains/feed/application/FeedService.java
@@ -2,12 +2,16 @@ package com.programmers.bucketback.domains.feed.application;
 
 import org.springframework.stereotype.Service;
 
+import com.programmers.bucketback.common.cursor.CursorPageParameters;
+import com.programmers.bucketback.common.cursor.CursorSummary;
 import com.programmers.bucketback.domains.feed.implementation.FeedAppender;
 import com.programmers.bucketback.domains.feed.implementation.FeedCursorReader;
 import com.programmers.bucketback.domains.feed.implementation.FeedModifier;
 import com.programmers.bucketback.domains.feed.implementation.FeedReader;
 import com.programmers.bucketback.domains.feed.implementation.FeedRemover;
 import com.programmers.bucketback.domains.feed.model.FeedCreateServiceRequest;
+import com.programmers.bucketback.domains.feed.model.FeedCursorSummaryLike;
+import com.programmers.bucketback.domains.feed.model.FeedSortCondition;
 import com.programmers.bucketback.domains.feed.model.FeedUpdateServiceRequest;
 import com.programmers.bucketback.global.util.MemberUtils;
 
@@ -39,23 +43,23 @@ public class FeedService {
 		feedModifier.modify(memberId, feedId, request);
 	}
 
-	// public FeedGetByCursorServiceResponse getFeedByCursor(
-	// 	final String hobbyName,
-	// 	final String nickName,
-	// 	final String sortCondition,
-	// 	final CursorPageParameters parameters
-	// ) {
-	// 	FeedSortCondition feedSortCondition = FeedSortCondition.from(sortCondition);
-	// 	Long loginMemberId = MemberUtils.getCurrentMemberId();
-	//
-	// 	return feedCursorReader.getFeedByCursor(
-	// 		hobbyName,
-	// 		nickName,
-	// 		loginMemberId,
-	// 		feedSortCondition,
-	// 		parameters
-	// 	);
-	// }
+	public CursorSummary<FeedCursorSummaryLike> getFeedByCursor(
+		final String hobbyName,
+		final String nickName,
+		final String sortCondition,
+		final CursorPageParameters parameters
+	) {
+		FeedSortCondition feedSortCondition = FeedSortCondition.from(sortCondition);
+		Long loginMemberId = MemberUtils.getCurrentMemberId();
+
+		return feedCursorReader.getFeedByCursor(
+			hobbyName,
+			nickName,
+			loginMemberId,
+			feedSortCondition,
+			parameters
+		);
+	}
 
 	/** 피드 삭제 */
 	public void deleteFeed(final Long feedId) {
