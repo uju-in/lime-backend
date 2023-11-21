b312cc0cefc18eb6179ae4af777e8ff7054b3742
JoJaeHyeon <joje1003@naver.com>
2023-11-21 18:17:22 +0900

    refactor : 피드 목록 조회 반환 타입 수정

--- bucketback-api/src/main/java/com/programmers/bucketback/domains/comment/api/dto/response/CommentGetCursorResponse.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/domains/comment/api/dto/response/CommentGetCursorResponse.java
@@ -1,18 +1,20 @@
 package com.programmers.bucketback.domains.comment.api.dto.response;
 
+import java.util.List;
+
 import com.programmers.bucketback.common.cursor.CursorSummary;
 import com.programmers.bucketback.domains.comment.repository.CommentSummary;
 
 public record CommentGetCursorResponse(
 	String nextCursorId,
 	int totalCount,
-	CursorSummary<CommentSummary> comments
+	List<CommentSummary> comments
 ) {
-	public static CommentGetCursorResponse from(final CursorSummary<CommentSummary> summaries) {
+	public static CommentGetCursorResponse from(final CursorSummary<CommentSummary> summary) {
 		return new CommentGetCursorResponse(
-			summaries.nextCursorId(),
-			summaries.summaryCount(),
-			summaries
+			summary.nextCursorId(),
+			summary.summaryCount(),
+			summary.summaries()
 		);
 	}
 }
