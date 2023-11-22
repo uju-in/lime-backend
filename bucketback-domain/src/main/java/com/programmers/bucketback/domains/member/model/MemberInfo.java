0715024e4701e020c7df175444700a9e2f6d5d70
Curry4182 <kangbk4182@naver.com>
2023-11-22 16:01:18 +0900

    refactor: 프로필 이미지 추가로 사용하지 않은 생성자 삭제

--- bucketback-domain/src/main/java/com/programmers/bucketback/domains/member/model/MemberInfo.java
+++ bucketback-domain/src/main/java/com/programmers/bucketback/domains/member/model/MemberInfo.java
@@ -1,6 +1,5 @@
 package com.programmers.bucketback.domains.member.model;
 
-import com.programmers.bucketback.domains.member.domain.Level;
 import com.programmers.bucketback.domains.member.domain.Member;
 
 import lombok.Builder;
@@ -12,15 +11,11 @@ public record MemberInfo(
 	String profileImage,
 	int level
 ) {
-	public MemberInfo(
-		final Long memberId,
-		final String nickName,
-		final int levelPoint
-	) {
-		this(memberId, nickName, null, Level.from(levelPoint));
-	}
-
 	public static MemberInfo from(final Member member) {
-		return new MemberInfo(member.getId(), member.getNickname(), member.getLevel());
+		return new MemberInfo(
+			member.getId(),
+			member.getNickname(),
+			member.getProfileImage(),
+			member.getLevel());
 	}
 }
