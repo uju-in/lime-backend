d4f570169587a683b7c4dfbb6b29dffa63d32e09
Curry4182 <kangbk4182@naver.com>
2023-11-22 23:15:31 +0900

    refactor: 서비스 dto에서 사용하지 않는 정보 삭제

--- bucketback-api/src/main/java/com/programmers/bucketback/domains/review/api/dto/response/ReviewGetResponse.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/domains/review/api/dto/response/ReviewGetResponse.java
@@ -1,26 +1,17 @@
 package com.programmers.bucketback.domains.review.api.dto.response;
 
-import java.time.LocalDateTime;
-
-import com.programmers.bucketback.domains.member.model.MemberInfo;
 import com.programmers.bucketback.domains.review.application.dto.ReviewGetServiceResponse;
 
 public record ReviewGetResponse(
 	Long reviewId,
 	int rate,
-	String content,
-	LocalDateTime createdAt,
-	LocalDateTime updatedAt,
-	MemberInfo memberInfo
+	String content
 ) {
 	public static ReviewGetResponse from(final ReviewGetServiceResponse serviceResponse) {
 		return new ReviewGetResponse(
 			serviceResponse.reviewId(),
 			serviceResponse.rate(),
-			serviceResponse.content(),
-			serviceResponse.createdAt(),
-			serviceResponse.updatedAt(),
-			serviceResponse.memberInfo()
+			serviceResponse.content()
 		);
 	}
 }
