455af58e8126bd7468e394c88d4808f7aae84397
Curry4182 <kangbk4182@naver.com>
2023-11-23 18:25:34 +0900

    feat: Item 생성을 위한 fixture 추가

---
+++ bucketback-domain/src/testFixtures/java/com/programmers/bucketback/item/builder/domain/ItemBuilder.java
@@ -0,0 +1,44 @@
+package com.programmers.bucketback.item.builder.domain;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.programmers.bucketback.common.model.Hobby;
+import com.programmers.bucketback.domains.item.domain.Item;
+import com.programmers.bucketback.domains.item.model.ItemCrawlerInfo;
+
+public class ItemBuilder {
+
+	public static Item successBuild() {
+		Item item = itemWithURLBuilder()
+			.price(10000)
+			.name("아이템")
+			.build();
+
+		ReflectionTestUtils.setField(
+			item,
+			"id",
+			1L
+		);
+
+		return item;
+	}
+
+	public static Item.ItemBuilder itemWithURLBuilder() {
+		return Item.builder()
+			.url("https://www.naver.com")
+			.image("https://www.naver.com//image");
+	}
+
+	public static Item fromItemCrawlerInfoBuild(
+		final Hobby hobby,
+		final ItemCrawlerInfo itemCrawlerInfo
+	) {
+		return Item.builder()
+			.hobby(Hobby.BASEBALL)
+			.url(itemCrawlerInfo.url())
+			.price(itemCrawlerInfo.price())
+			.name(itemCrawlerInfo.itemName())
+			.image(itemCrawlerInfo.imageUrl())
+			.build();
+	}
+}
