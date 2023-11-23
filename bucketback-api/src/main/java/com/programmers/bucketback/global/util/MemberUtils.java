ceb0b4d6ff131df628ee7c4de4b5ba15af00ac63
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-23 14:57:21 +0900

    refactor: 로그인한 회원 조회 시 탈퇴 회원인지 검증하는 로직 추가

--- bucketback-api/src/main/java/com/programmers/bucketback/global/util/MemberUtils.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/global/util/MemberUtils.java
@@ -1,14 +1,46 @@
 package com.programmers.bucketback.global.util;
 
+import org.springframework.stereotype.Component;
+
+import com.programmers.bucketback.domains.member.domain.Member;
+import com.programmers.bucketback.domains.member.implementation.MemberReader;
+import com.programmers.bucketback.error.BusinessException;
+import com.programmers.bucketback.error.ErrorCode;
 import com.programmers.bucketback.global.config.security.SecurityUtils;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
+@RequiredArgsConstructor
 public final class MemberUtils {
 
-	public static Long getCurrentMemberId() {
-		return SecurityUtils.getCurrentMemberId();
+	private final MemberReader memberReader;
+
+	public Long getCurrentMemberId() {
+		final Long memberId = SecurityUtils.getCurrentMemberId();
+
+		if (memberId == null) {
+			return null;
+		}
+
+		readMember(memberId);
+
+		return memberId;
+	}
+
+	public Member getCurrentMember() {
+		final Long memberId = SecurityUtils.getCurrentMemberId();
+
+		return readMember(memberId);
+	}
+
+	private Member readMember(final Long memberId) {
+		final Member member = memberReader.read(memberId);
+
+		if (member.isDeleted()) {
+			throw new BusinessException(ErrorCode.MEMBER_DELETED);
+		}
+
+		return member;
 	}
 }
