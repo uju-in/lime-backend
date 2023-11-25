ae17cff0371d3b04caa08a45e32011983a0ab4c2
Curry4182 <kangbk4182@naver.com>
2023-11-25 22:45:45 +0900

    test: 기본값으로 나의 아이템 목록 조회 테스트

--- bucketback-domain/src/test/java/com/programmers/bucketback/domains/item/implementation/MemberItemReaderTest.java
+++ bucketback-domain/src/test/java/com/programmers/bucketback/domains/item/implementation/MemberItemReaderTest.java
@@ -6,6 +6,7 @@ import static org.mockito.BDDMockito.*;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -30,34 +31,70 @@ class MemberItemReaderTest {
 	@InjectMocks
 	private MemberItemReader memberItemReader;
 
-	@Test
-	@DisplayName("커서를 이용해 나의 아이템 목록 조회한다.")
-	void readByCursor() {
-		// given
-		Hobby hobby = Hobby.BASEBALL;
-		Long memberId = 1L;
-		CursorPageParameters parameters = CursorPageParametersBuilder.build();
-		List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
-		CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
+	@Nested
+	@DisplayName("나의 아이템 목록 조회 테스트")
+	class MemberItemReadTest {
+		@Test
+		@DisplayName("커서를 이용해 나의 아이템 목록 조회한다.")
+		void readByCursor() {
+			// given
+			Hobby hobby = Hobby.BASEBALL;
+			Long memberId = 1L;
+			CursorPageParameters parameters = CursorPageParametersBuilder.build();
+			List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
+			CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
 
-		given(memberItemRepository.findMemberItemsByCursor(
+			given(memberItemRepository.findMemberItemsByCursor(
+					hobby,
+					memberId,
+					parameters.cursorId(),
+					parameters.size()
+				)
+			).willReturn(memberItemSummaries);
+
+			// when
+			CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
 				hobby,
 				memberId,
-				parameters.cursorId(),
-				parameters.size()
-			)
-		).willReturn(memberItemSummaries);
+				parameters
+			);
 
-		// when
-		CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
-			hobby,
-			memberId,
-			parameters
-		);
+			// then
+			assertThat(actualCursorSummary)
+				.usingRecursiveComparison()
+				.isEqualTo(cursorSummaries);
+		}
 
-		// then
-		assertThat(actualCursorSummary)
-			.usingRecursiveComparison()
-			.isEqualTo(cursorSummaries);
+		@Test
+		@DisplayName("CursorId와 size가 null일 때 기본값으로 조회한다.")
+		void readByCursorWithNullCursorIdAndSize() {
+			// given
+			Hobby hobby = Hobby.BASEBALL;
+			Long memberId = 1L;
+			CursorPageParameters parameters = CursorPageParametersBuilder.buildWithNull();
+			List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
+			CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
+
+			given(memberItemRepository.findMemberItemsByCursor(
+					hobby,
+					memberId,
+					null,
+					20
+				)
+			).willReturn(memberItemSummaries);
+
+			// when
+			CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
+				hobby,
+				memberId,
+				parameters
+			);
+
+			// then
+			assertThat(actualCursorSummary)
+				.usingRecursiveComparison()
+				.isEqualTo(cursorSummaries);
+		}
 	}
+
 }
