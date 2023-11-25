e9b1fdd91055f8842f5471b99318226825caf258
Curry4182 <kangbk4182@naver.com>
2023-11-26 05:17:03 +0900

    test: 테스트할 필요가 없는 클래스에 Disabled 어노테이션 추가

--- bucketback-api/src/test/java/com/programmers/bucketback/IntegrationTest.java
+++ bucketback-api/src/test/java/com/programmers/bucketback/IntegrationTest.java
@@ -1,9 +1,11 @@
 package com.programmers.bucketback;
 
+import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 @Sql(scripts = {"classpath:truncate.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
