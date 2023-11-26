3d7bcf7340088371612ba5e3c499d9fc6f7a39f2
Curry4182 <kangbk4182@naver.com>
2023-11-26 23:03:16 +0900

    test: ParameterizedTest로 입력에 따라 아이템 목록 테스트할 수 있도록 수정

--- bucketback-domain/src/test/java/com/programmers/bucketback/domains/item/implementation/MemberItemReaderTest.java
+++ bucketback-domain/src/test/java/com/programmers/bucketback/domains/item/implementation/MemberItemReaderTest.java
@@ -4,11 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -25,76 +27,49 @@ import com.programmers.bucketback.domains.item.repository.MemberItemRepository;
 @ExtendWith(MockitoExtension.class)
 class MemberItemReaderTest {
 
+	private static final int DEFAULT_PAGE_SIZE = 20;
 	@Mock
 	private MemberItemRepository memberItemRepository;
-
 	@InjectMocks
 	private MemberItemReader memberItemReader;
 
-	@Nested
-	@DisplayName("나의 아이템 목록 조회 테스트")
-	class MemberItemReadTest {
-		@Test
-		@DisplayName("커서를 이용해 나의 아이템 목록 조회한다.")
-		void readByCursor() {
-			// given
-			Hobby hobby = Hobby.BASEBALL;
-			Long memberId = 1L;
-			CursorPageParameters parameters = CursorPageParametersBuilder.build();
-			List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
-			CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
+	static Stream<Arguments> provideParameters() {
+		return Stream.of(
+			Arguments.of(CursorPageParametersBuilder.build()),
+			Arguments.of(CursorPageParametersBuilder.buildWithNull())
+		);
+	}
 
-			given(memberItemRepository.findMemberItemsByCursor(
-					hobby,
-					memberId,
-					parameters.cursorId(),
-					parameters.size()
-				)
-			).willReturn(memberItemSummaries);
+	@ParameterizedTest
+	@MethodSource("provideParameters")
+	@DisplayName("커서를 이용해 나의 아이템 목록 조회한다.")
+	void readByCursor(final CursorPageParameters parameters) {
+		// given
+		Hobby hobby = Hobby.BASEBALL;
+		Long memberId = 1L;
+		List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
+		CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
 
-			// when
-			CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
+		int pageSize = parameters.size() == null ? DEFAULT_PAGE_SIZE : parameters.size();
+		given(memberItemRepository.findMemberItemsByCursor(
 				hobby,
 				memberId,
-				parameters
-			);
+				parameters.cursorId(),
+				pageSize
+			)
+		).willReturn(memberItemSummaries);
 
-			// then
-			assertThat(actualCursorSummary)
-				.usingRecursiveComparison()
-				.isEqualTo(cursorSummaries);
-		}
+		// when
+		CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
+			hobby,
+			memberId,
+			parameters
+		);
 
-		@Test
-		@DisplayName("CursorId와 size가 null일 때 기본값으로 조회한다.")
-		void readByCursorWithNullCursorIdAndSize() {
-			// given
-			Hobby hobby = Hobby.BASEBALL;
-			Long memberId = 1L;
-			CursorPageParameters parameters = CursorPageParametersBuilder.buildWithNull();
-			List<MemberItemSummary> memberItemSummaries = MemberItemSummaryBuilder.buildMany();
-			CursorSummary<MemberItemSummary> cursorSummaries = CursorUtils.getCursorSummaries(memberItemSummaries);
-
-			given(memberItemRepository.findMemberItemsByCursor(
-					hobby,
-					memberId,
-					null,
-					20
-				)
-			).willReturn(memberItemSummaries);
-
-			// when
-			CursorSummary<MemberItemSummary> actualCursorSummary = memberItemReader.readMemberItem(
-				hobby,
-				memberId,
-				parameters
-			);
-
-			// then
-			assertThat(actualCursorSummary)
-				.usingRecursiveComparison()
-				.isEqualTo(cursorSummaries);
-		}
+		// then
+		assertThat(actualCursorSummary)
+			.usingRecursiveComparison()
+			.isEqualTo(cursorSummaries);
 	}
 
 }
