70f5a7ec7350f4f546b15183db8f935a0c880447
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-26 23:52:35 +0900

    fixture: final 추가, buildMany range 변경

--- bucketback-domain/src/testFixtures/java/com/programmers/bucketback/domains/vote/domain/VoterBuilder.java
+++ bucketback-domain/src/testFixtures/java/com/programmers/bucketback/domains/vote/domain/VoterBuilder.java
@@ -11,14 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VoterBuilder {
 	public static List<Voter> buildMany(
-		final int size,
+		final long size,
 		final Vote vote,
 		final Long itemId
 	) {
-		return LongStream.range(0, size)
+		return LongStream.range(1, size + 1)
 			.mapToObj(i -> {
-				Voter voter = VoterBuilder.build(vote, i + 1, itemId);
-				setVoterId(voter, i + 1);
+				Voter voter = VoterBuilder.build(vote, i, itemId);
+				setVoterId(voter, i);
 
 				return voter;
 			})
@@ -30,7 +30,7 @@ public class VoterBuilder {
 		final Long memberId,
 		final Long itemId
 	) {
-		Voter voter = new Voter(vote, memberId, itemId);
+		final Voter voter = new Voter(vote, memberId, itemId);
 
 		setVoterId(voter, 1L);
 
