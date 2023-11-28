e673ad982e3ed8624360725bae447b94dc135d1c
Curry4182 <kangbk4182@naver.com>
2023-11-28 21:36:58 +0900

    refactor: 검증 되지 않는 커밋 취소

--- bucketback-api/src/main/java/com/programmers/bucketback/domains/member/api/MemberController.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/domains/member/api/MemberController.java
@@ -2,7 +2,6 @@ package com.programmers.bucketback.domains.member.api;
 
 import java.io.IOException;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +33,6 @@ import com.programmers.bucketback.domains.member.model.MyPage;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -120,13 +118,10 @@ public class MemberController {
 	@Operation(summary = "마이페이지 조회", description = "닉네임을 이용하여 마이페이지를 조회합니다.")
 	@GetMapping("/{nickname}")
 	public ResponseEntity<MemberGetMyPageResponse> getMyPage(
-		@PathVariable final String nickname,
-		final HttpServletResponse servletResponse
+		@PathVariable final String nickname
 	) {
 		MyPage myPage = memberService.getMyPage(nickname);
 		MemberGetMyPageResponse response = MemberGetMyPageResponse.from(myPage);
-		servletResponse.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0, must-revalidate");
-		servletResponse.setHeader("Pragma", "no-cache");
 
 		return ResponseEntity.ok(response);
 	}
