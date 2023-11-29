1c208e6d64ef3d3c198616d287e7812fa3e48e9a
Curry4182 <kangbk4182@naver.com>
2023-11-30 06:44:42 +0900

    feat: 피드 랭킹 정보를 레디스에 저장 하는 기능 추가

---
+++ bucketback-infrastructure/src/main/java/com/programmers/bucketback/redis/feed/FeedRedisManager.java
@@ -0,0 +1,37 @@
+package com.programmers.bucketback.redis.feed;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Component;
+
+import com.programmers.bucketback.redis.feed.model.FeedRankingInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class FeedRedisManager {
+
+	private static final String FEED_RANKING_INFO_SET_KEY = "feedRankingInfo";
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public void increasePopularity(final FeedRankingInfo feedRankingInfo) {
+		redisTemplate.opsForZSet().incrementScore(FEED_RANKING_INFO_SET_KEY, feedRankingInfo, 1);
+	}
+
+	public List<FeedRankingInfo> getFeedRanking() {
+		ZSetOperations<String, Object> ZSetOperations = redisTemplate.opsForZSet();
+
+		Set<ZSetOperations.TypedTuple<Object>> feedRankingInfoSet =
+			ZSetOperations.reverseRangeWithScores(
+				FEED_RANKING_INFO_SET_KEY, 0, 9
+			);
+
+		return feedRankingInfoSet.stream()
+			.map(FeedRankingInfo::of)
+			.toList();
+	}
+}
