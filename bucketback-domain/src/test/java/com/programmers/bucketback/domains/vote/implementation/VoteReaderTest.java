37a31d2ad6df3e8b3e0d7072aeb264793343d1ed
Yiseul Park <omjl5123@catholic.ac.kr>
2023-11-29 14:59:56 +0900

    test: 투표 상세 조회 테스트 추가

--- bucketback-domain/src/test/java/com/programmers/bucketback/domains/vote/implementation/VoteReaderTest.java
+++ bucketback-domain/src/test/java/com/programmers/bucketback/domains/vote/implementation/VoteReaderTest.java
@@ -13,9 +13,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.programmers.bucketback.domains.item.domain.Item;
+import com.programmers.bucketback.domains.item.domain.ItemBuilder;
 import com.programmers.bucketback.domains.item.implementation.ItemReader;
+import com.programmers.bucketback.domains.item.model.ItemInfo;
 import com.programmers.bucketback.domains.vote.domain.Vote;
 import com.programmers.bucketback.domains.vote.domain.VoteBuilder;
+import com.programmers.bucketback.domains.vote.model.VoteDetail;
+import com.programmers.bucketback.domains.vote.model.VoteDetailInfo;
 import com.programmers.bucketback.domains.vote.repository.VoteRepository;
 import com.programmers.bucketback.error.EntityNotFoundException;
 
@@ -71,5 +76,46 @@ class VoteReaderTest {
 				() -> voteReader.read(wrongVoteId)
 			).isInstanceOf(EntityNotFoundException.class);
 		}
+	}
+
+	@Test
+	@DisplayName("투표 상세 조회한다.")
+	void readDetailTest() {
+		// given
+		final Long memberId = 1L;
+		final Vote vote = VoteBuilder.build();
+		final Long item1Id = vote.getItem1Id();
+		final Long item2Id = vote.getItem2Id();
+
+		final Item item1 = ItemBuilder.build(item1Id);
+		final Item item2 = ItemBuilder.build(item2Id);
+		final ItemInfo item1Info = ItemInfo.from(item1);
+		final ItemInfo item2Info = ItemInfo.from(item2);
+
+		final int item1Votes = 4;
+		final int item2Votes = 5;
+		final VoteDetailInfo voteInfo = VoteDetailInfo.of(vote, item1Votes, item2Votes);
+
+		given(voteRepository.findById(anyLong()))
+			.willReturn(Optional.of(vote));
+		given(itemReader.read(item1Id))
+			.willReturn(item1);
+		given(itemReader.read(item2Id))
+			.willReturn(item2);
+		given(voteCounter.count(any(Vote.class), eq(item1Id)))
+			.willReturn(item1Votes);
+		given(voteCounter.count(any(Vote.class), eq(item2Id)))
+			.willReturn(item2Votes);
+		given(voterReader.readItemId(any(Vote.class), anyLong()))
+			.willReturn(item1Id);
+
+		// when
+		final VoteDetail result = voteReader.readDetail(vote.getId(), memberId);
+
+		// then
+		assertThat(result.voteInfo()).isEqualTo(voteInfo);
+		assertThat(result.item1Info()).isEqualTo(item1Info);
+		assertThat(result.item2Info()).isEqualTo(item2Info);
+		assertThat(result.selectedItemId()).isEqualTo(item1Id);
 	}
 }
