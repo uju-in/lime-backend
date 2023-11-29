cdd85fa9430152bbb45d8624419e7b6485cc145d
JoJaeHyeon <joje1003@naver.com>
2023-11-30 00:23:34 +0900

    refactor : 정적 팩토리 메서드 > new 메소드로 변경

--- bucketback-api/src/main/java/com/programmers/bucketback/domains/item/application/dto/MemberItemGetServiceResponse.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/domains/item/application/dto/MemberItemGetServiceResponse.java
@@ -7,10 +7,4 @@ public record MemberItemGetServiceResponse(
 	CursorSummary<MemberItemSummary> cursorSummary,
 	int totalMemberItemCount
 ) {
-	public static MemberItemGetServiceResponse of(
-		final CursorSummary<MemberItemSummary> cursorSummary,
-		final int totalMemberItemCount
-	) {
-		return new MemberItemGetServiceResponse(cursorSummary, totalMemberItemCount);
-	}
 }
