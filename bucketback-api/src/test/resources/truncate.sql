f600b8aae0e61a513c0d12f636315ed89d235cb1
Curry4182 <kangbk4182@naver.com>
2023-11-30 04:47:20 +0900

    feat: 테스트 하기전 테이블 삭제 하기 위한 sql 추가

---
+++ bucketback-api/src/test/resources/truncate.sql
@@ -0,0 +1,17 @@
+set
+FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE bucket_items;
+TRUNCATE TABLE feed_items;
+TRUNCATE TABLE feed_likes;
+TRUNCATE TABLE inventory_items;
+TRUNCATE TABLE members_items;
+TRUNCATE TABLE voters;
+TRUNCATE TABLE buckets;
+TRUNCATE TABLE comments;
+TRUNCATE TABLE feeds;
+TRUNCATE TABLE inventories;
+TRUNCATE TABLE reviews;
+TRUNCATE TABLE votes;
+TRUNCATE TABLE items;
+TRUNCATE TABLE members;
+set
