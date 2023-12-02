7ad3a2a3a4904968afa141d0644baa0b7cfaf4c6
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-02 16:24:06 +0900

    fix: 잘못된 네이밍 수정

--- bucketback-api/src/main/java/com/programmers/bucketback/domains/vote/api/dto/response/VoteGetByKeywordResponse.java
+++ bucketback-api/src/main/java/com/programmers/bucketback/domains/vote/api/dto/response/VoteGetByKeywordResponse.java
@@ -8,7 +8,7 @@ import com.programmers.bucketback.domains.vote.model.VoteSummary;
 public record VoteGetByKeywordResponse(
 	String nextCursorId,
 	int totalCount,
-	long totalCommentCount,
+	long totalVoteCount,
 	List<VoteSummary> votes
 ) {
 	public static VoteGetByKeywordResponse from(final VoteGetByKeywordServiceResponse response) {
