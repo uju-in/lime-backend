eb24b1d1e8add1bd92256b0a4ab1a11d730cc109
Yiseul Park <omjl5123@catholic.ac.kr>
2023-12-29 16:13:25 +0900

    feat: cache 관련 설정 추가

---
+++ bucketback-api/src/main/java/com/programmers/bucketback/global/cache/CacheConfiguration.java
@@ -0,0 +1,33 @@
+package com.programmers.bucketback.global.cache;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@EnableCaching
+@Configuration
+public class CacheConfiguration {
+	@Bean
+	public CacheManager cacheManager() {
+		final List<CaffeineCache> caffeineCaches = Arrays.stream(CacheType.values())
+			.map(cache -> new CaffeineCache(cache.getCacheName(), Caffeine.newBuilder().recordStats()
+				.expireAfterWrite(cache.getExpireAfterWrite(), TimeUnit.HOURS)
+				.maximumSize(cache.getEntryMaxSize())
+				.build()))
+			.toList();
+
+		final SimpleCacheManager cacheManager = new SimpleCacheManager();
+		cacheManager.setCaches(caffeineCaches);
+
+		return cacheManager;
+	}
+}
