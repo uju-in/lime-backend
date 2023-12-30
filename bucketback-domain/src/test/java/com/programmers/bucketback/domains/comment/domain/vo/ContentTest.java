53584ba03b0f67ea6aa3bdc70ce65055c31c81a4
JoJaeHyeon <joje1003@naver.com>
2023-12-31 01:18:11 +0900

    chore : 테스트 이름 설명 추가

--- bucketback-domain/src/test/java/com/programmers/bucketback/domains/comment/domain/vo/ContentTest.java
+++ bucketback-domain/src/test/java/com/programmers/bucketback/domains/comment/domain/vo/ContentTest.java
@@ -2,6 +2,7 @@ package com.programmers.bucketback.domains.comment.domain.vo;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -11,6 +12,7 @@ import com.programmers.bucketback.error.BusinessException;
 class ContentTest {
 
 	@ParameterizedTest
+	@DisplayName("댓글 내용 유효한 값의 길이 검증 성공")
 	@ValueSource(ints = {1, 10, 300})
 	void successValidateContentLength(int count) {
 		String fill = "a";
@@ -19,16 +21,18 @@ class ContentTest {
 	}
 
 	@ParameterizedTest
+	@DisplayName("댓글 내용 유효하지 않은 값 예외 검증 성공")
 	@ValueSource(ints = {301, 0})
-	void failValidateContentLength(int count) {
+	void successValidateInvalidContentLength(int count) {
 		String fill = "a";
 		assertThatThrownBy(() -> new Content(fill.repeat(count)))
 			.isInstanceOf(BusinessException.class);
 	}
 
 	@ParameterizedTest
+	@DisplayName("빈 내용과 null 값에 대한 예외 성공")
 	@NullAndEmptySource
-	void failEmptyAndNullContent(String fill) {
+	void successValidateEmptyAndNullContent(String fill) {
 		assertThatThrownBy(() -> new Content(fill))
 			.isInstanceOf(BusinessException.class);
 	}
