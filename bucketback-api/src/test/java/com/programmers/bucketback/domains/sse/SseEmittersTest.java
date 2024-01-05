db32aca96b2e342b6a15e160bf710f6457836f27
JoJaeHyeon <joje1003@naver.com>
2024-01-05 20:48:36 +0900

    test : 단위테스트 수정

--- bucketback-api/src/test/java/com/programmers/bucketback/domains/sse/SseEmittersTest.java
+++ bucketback-api/src/test/java/com/programmers/bucketback/domains/sse/SseEmittersTest.java
@@ -1,9 +1,12 @@
 package com.programmers.bucketback.domains.sse;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 class SseEmittersTest {
@@ -11,6 +14,7 @@ class SseEmittersTest {
 	SseEmitters sseEmitters = new SseEmitters();
 	Long memberId = 1L;
 	Long defaultTimeOut = 30 * 60 * 1000L;
+	Logger mockLogger = mock(Logger.class);
 
 	@Test
 	@DisplayName("새로운 Emitters를 추가에 성공한다.")
@@ -23,23 +27,20 @@ class SseEmittersTest {
 	}
 
 	@Test
-	@DisplayName("Emitters에 있는 정보를 가져올 수 있다.")
+	@DisplayName("Emitters에 정보가 존재한다.")
 	public void successGetEmitter() throws Exception {
 		//when
 		sseEmitters.add(memberId);
-		SseEmitter sseEmitter = sseEmitters.get(memberId);
 
 		//then
-		assertThat(sseEmitter).isNotNull();
+		verify(mockLogger, times(1));
+		assertDoesNotThrow(() -> sseEmitters.send(memberId, null));
 	}
 
 	@Test
 	@DisplayName("Emitters에 저장된 정보가 없다.")
 	public void failGetEmitter() throws Exception {
-		//when
-		SseEmitter sseEmitter = sseEmitters.get(memberId);
-
-		//then
-		assertThat(sseEmitter).isNull();
+		verify(mockLogger, times(1));
+		assertDoesNotThrow(() -> sseEmitters.send(memberId, null));
 	}
 }
