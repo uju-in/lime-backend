512a75eb7273c17deb8cff4d48560c28ca3794cd
JoJaeHyeon <joje1003@naver.com>
2024-01-07 18:43:27 +0900

    test : refresh Token 적용으로 인한 단위테스트 수정

--- bucketback-api/src/test/java/com/programmers/bucketback/global/config/security/jwt/JwtServiceTest.java
+++ bucketback-api/src/test/java/com/programmers/bucketback/global/config/security/jwt/JwtServiceTest.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import javax.crypto.SecretKey;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -14,17 +15,18 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import com.programmers.bucketback.domains.member.domain.Member;
 import com.programmers.bucketback.domains.member.domain.vo.Role;
-import com.programmers.bucketback.global.config.security.MemberSecurity;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 class JwtServiceTest {
 
-	private String secretKey = "testsecretkeydaslkjslsdnmflsdkgjsldgkjlweptojkgn";
-	private JwtConfig jwtConfig = new JwtConfig(secretKey, 100);
+	private String accessSecretKey = "testsecretkeydaslkjslsdnmflsdkgjsldgkjlweptojkgn";
+	private String refreshSecretKey = "testsecretkeydaslkjslsdnmflsdkgjsldgkjlweptojkgn";
+	private JwtConfig jwtConfig = new JwtConfig(accessSecretKey, 100, refreshSecretKey, 100);
 	private JwtService jwtService = new JwtService(jwtConfig);
 
 	private static Member buildMember() {
@@ -43,13 +45,12 @@ class JwtServiceTest {
 		//given
 		Member member = buildMember();
 		ReflectionTestUtils.setField(member, "id", 1L);
-		MemberSecurity memberSecurity = new MemberSecurity(member);
 
 		//when
-		String token = jwtService.generateToken(memberSecurity);
+		String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
 		String username = jwtService.extractUsername(token);
 
-		byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.secretKey());
+		byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.accessSecretKey());
 		SecretKey decodedSecretKey = Keys.hmacShaKeyFor(keyBytes);
 		Claims claims = Jwts.parserBuilder().setSigningKey(decodedSecretKey).build().parseClaimsJws(token).getBody();
 
@@ -58,42 +59,42 @@ class JwtServiceTest {
 		assertThat(username).isEqualTo(String.valueOf(member.getId()));
 	}
 
-	@ParameterizedTest
-	@ValueSource(ints = {1, 10, 1000})
-	@DisplayName("유효한 토큰 유효성 검사에 성공한다.")
-	void successValidTokenEvaluation(int expirationSecond) {
-		//given
-		String secretKey = "testsecretkeydaslkjslsdnmflsdkgjsldgkjlweptojkgn";
-		JwtConfig jwtConfig = new JwtConfig(secretKey, expirationSecond);
-		JwtService jwtService = new JwtService(jwtConfig);
+	@Nested
+	@DisplayName("유효한 토큰 유효성 검사")
+	class ValidAccessToken {
 
-		Member member = buildMember();
-		ReflectionTestUtils.setField(member, "id", 1L);
-		MemberSecurity memberSecurity = new MemberSecurity(member);
-		String token = jwtService.generateToken(memberSecurity);
+		@ParameterizedTest
+		@ValueSource(ints = {1, 10, 1000})
+		@DisplayName("유효한 토큰 검사에 성공한다.")
+		void successValidTokenEvaluation(int expirationSecond) {
+			//given
+			JwtConfig jwtConfig = new JwtConfig(accessSecretKey, expirationSecond, refreshSecretKey, 100);
+			JwtService jwtService = new JwtService(jwtConfig);
 
-		//given
-		boolean result = jwtService.isTokenValid(token, memberSecurity);
-		assertThat(result).isEqualTo(true);
-	}
+			Member member = buildMember();
+			ReflectionTestUtils.setField(member, "id", 1L);
+			String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
 
-	@ParameterizedTest
-	@ValueSource(ints = {0})
-	@DisplayName("유효하지 않은 토큰 유효성 검사에 성공한다.")
-	void successInValidTokenEvaluation() {
-		//given
-		String secretKey = "testsecretkeydaslkjslsdnmflsdkgjsldgkjlweptojkgn";
-		JwtConfig jwtConfig = new JwtConfig(secretKey, 0);
-		JwtService jwtService = new JwtService(jwtConfig);
+			//given
+			boolean result = jwtService.isAccessTokenValid(token);
+			assertThat(result).isEqualTo(true);
+		}
 
-		Member member = buildMember();
-		ReflectionTestUtils.setField(member, "id", 1L);
-		MemberSecurity memberSecurity = new MemberSecurity(member);
-		String token = jwtService.generateToken(memberSecurity);
+		@ParameterizedTest
+		@ValueSource(ints = {0})
+		@DisplayName("만료된 토큰 검증에 성공한다.")
+		void successExpiredTokenEvaluation(int expirationSecond) {
+			//given
+			JwtConfig jwtConfig = new JwtConfig(accessSecretKey, expirationSecond, refreshSecretKey, 100);
+			JwtService jwtService = new JwtService(jwtConfig);
 
-		//given
-		boolean result = jwtService.isTokenValid(token, memberSecurity);
-		assertThat(result).isEqualTo(false);
+			Member member = buildMember();
+			ReflectionTestUtils.setField(member, "id", 1L);
+			String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
+
+			//given
+			assertThatThrownBy(() -> jwtService.isAccessTokenValid(token)).isInstanceOf(JwtException.class);
+		}
 	}
 
 }
