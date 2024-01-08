ff18c37d6d21cf9dd761a710bb3dc211474b4d15
JoJaeHyeon <joje1003@naver.com>
2024-01-08 16:49:19 +0900

    test : 피드 상세 조회 로직 memberBuilder 삭제, private메소드 => public메소드 테스트로 수정함

--- bucketback-api/src/test/java/com/programmers/bucketback/global/config/security/jwt/JwtServiceTest.java
+++ bucketback-api/src/test/java/com/programmers/bucketback/global/config/security/jwt/JwtServiceTest.java
@@ -2,25 +2,16 @@ package com.programmers.bucketback.global.config.security.jwt;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Date;
-
-import javax.crypto.SecretKey;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import com.programmers.bucketback.domains.member.domain.Member;
-import com.programmers.bucketback.domains.member.domain.vo.Role;
+import com.programmers.bucketback.domains.member.domain.MemberBuilder;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 
 class JwtServiceTest {
 
@@ -29,33 +20,19 @@ class JwtServiceTest {
 	private JwtConfig jwtConfig = new JwtConfig(accessSecretKey, 100, refreshSecretKey, 100);
 	private JwtService jwtService = new JwtService(jwtConfig);
 
-	private static Member buildMember() {
-		Member member = Member.builder()
-			.email("email@email.com")
-			.password("password!123")
-			.nickname("nickname")
-			.role(Role.USER)
-			.build();
-		return member;
-	}
-
 	@Test
 	@DisplayName("access token 생성에 성공한다.")
 	void successAccessTokenCreate() {
 		//given
-		Member member = buildMember();
-		ReflectionTestUtils.setField(member, "id", 1L);
+		Long memberId = 1L;
+		Member member = MemberBuilder.build(memberId);
 
 		//when
 		String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
 		String username = jwtService.extractUsername(token);
 
-		byte[] keyBytes = Decoders.BASE64.decode(jwtConfig.accessSecretKey());
-		SecretKey decodedSecretKey = Keys.hmacShaKeyFor(keyBytes);
-		Claims claims = Jwts.parserBuilder().setSigningKey(decodedSecretKey).build().parseClaimsJws(token).getBody();
-
 		//then
-		assertThat(claims.getExpiration()).isAfter(new Date());
+		assertThat(jwtService.isAccessTokenValid(token)).isEqualTo(true);
 		assertThat(username).isEqualTo(String.valueOf(member.getId()));
 	}
 
@@ -71,8 +48,8 @@ class JwtServiceTest {
 			JwtConfig jwtConfig = new JwtConfig(accessSecretKey, expirationSecond, refreshSecretKey, 100);
 			JwtService jwtService = new JwtService(jwtConfig);
 
-			Member member = buildMember();
-			ReflectionTestUtils.setField(member, "id", 1L);
+			Long memberId = 1L;
+			Member member = MemberBuilder.build(memberId);
 			String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
 
 			//given
@@ -88,8 +65,8 @@ class JwtServiceTest {
 			JwtConfig jwtConfig = new JwtConfig(accessSecretKey, expirationSecond, refreshSecretKey, 100);
 			JwtService jwtService = new JwtService(jwtConfig);
 
-			Member member = buildMember();
-			ReflectionTestUtils.setField(member, "id", 1L);
+			Long memberId = 1L;
+			Member member = MemberBuilder.build(memberId);
 			String token = jwtService.generateAccessToken(String.valueOf(member.getId()));
 
 			//given
