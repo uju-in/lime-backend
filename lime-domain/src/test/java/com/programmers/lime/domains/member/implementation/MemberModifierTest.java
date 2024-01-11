54281acdfa6b71f2b2ea5056b7a00925cd9f4f51
Yiseul Park <omjl5123@catholic.ac.kr>
2024-01-11 16:46:26 +0900

    test: 변경된 프로필 업데이트 기능에 맞게 테스트 수정

--- lime-domain/src/test/java/com/programmers/lime/domains/member/implementation/MemberModifierTest.java
+++ lime-domain/src/test/java/com/programmers/lime/domains/member/implementation/MemberModifierTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.programmers.lime.domains.member.domain.Member;
 import com.programmers.lime.domains.member.domain.MemberBuilder;
+import com.programmers.lime.domains.member.domain.vo.Introduction;
 
 @ExtendWith(MockitoExtension.class)
 class MemberModifierTest {
@@ -28,21 +29,27 @@ class MemberModifierTest {
 
 	@ParameterizedTest
 	@CsvSource({"nickname, 0", "best_park, 1"})
-	@DisplayName("주어진 닉네임과 자기소개로 프로필을 수정한다.")
+	@DisplayName("프로필을 수정한다.")
 	void modifyProfileTest(
 		final String nickname,
 		final int time
 	) {
 		// given
 		final Member member = MemberBuilder.build(1L);
-		final String introduction = "안녕하세요!";
+		final Introduction introduction = Introduction.builder()
+			.hobby("basketball")
+			.career(2)
+			.favorability("관심")
+			.content("안녕하세요!")
+			.mbti("ISFJ")
+			.build();
 
 		// when
 		memberModifier.modifyProfile(member, nickname, introduction);
 
 		// then
 		assertThat(member.getNickname()).isEqualTo(nickname);
-		assertThat(member.getIntroduction()).isEqualTo(introduction);
+		assertThat(member.getIntroduction()).usingRecursiveComparison().isEqualTo(introduction);
 		then(memberChecker).should(times(time)).checkNicknameDuplication(nickname);
 	}
 
