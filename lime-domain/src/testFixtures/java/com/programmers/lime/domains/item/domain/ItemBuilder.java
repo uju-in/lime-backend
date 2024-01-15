86c3aa6a857fc168b7084ef6db686c4c9c1f9cd4
Curry4182 <kangbk4182@naver.com>
2024-01-15 16:45:06 +0900

    test: 아이템 빌더 폴더 아이디 추가

--- lime-domain/src/testFixtures/java/com/programmers/lime/domains/item/domain/ItemBuilder.java
+++ lime-domain/src/testFixtures/java/com/programmers/lime/domains/item/domain/ItemBuilder.java
@@ -45,16 +45,19 @@ public class ItemBuilder {
 			.url("https://www.naver.com")
 			.image("https://www.naver.com//image")
 			.price(10000)
-			.hobby(Hobby.SWIMMING)
+			.hobby(Hobby.BASEBALL)
+			.folderId(1L)
 			.name("아이템");
 	}
 
 	public static Item fromItemCrawlerInfoBuild(
 		final Hobby hobby,
+		final Long folderId,
 		final ItemCrawlerInfo itemCrawlerInfo
 	) {
 		Item item = Item.builder()
 			.hobby(hobby)
+			.folderId(folderId)
 			.url(itemCrawlerInfo.url())
 			.price(itemCrawlerInfo.price())
 			.name(itemCrawlerInfo.itemName())
