121036eedf7ae83dd89e2144c88d55cf5a267d86
JoJaeHyeon <joje1003@naver.com>
2024-01-15 20:36:37 +0900

    chore : 소셜로그인 적용으로 사용하지 않는 클래스 제거

--- lime-api/src/main/java/com/programmers/lime/domains/member/api/dto/response/MemberCheckEmailResponse.java
+++
@@ -1,6 +0,0 @@
-package com.programmers.lime.domains.member.api.dto.response;
-
-public record MemberCheckEmailResponse(
-	String code
-) {
-}
--- lime-api/src/main/java/com/programmers/lime/domains/member/application/dto/response/MemberLoginServiceResponse.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.lime.domains.member.application.dto.response;
-
-public record MemberLoginServiceResponse(
-	Long memberId,
-	String nickname,
-	String accessToken,
-	String refreshToken
-) {
-}
