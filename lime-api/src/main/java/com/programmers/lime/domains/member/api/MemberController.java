cb7250e622e1b061939d5ea4ac6713c6a2ef4b32
JoJaeHyeon <joje1003@naver.com>
2024-01-15 20:44:44 +0900

    refactor : 소셜로그인 반영으로 인한 일부 로직 수정 (service, memberContoller)

--- lime-api/src/main/java/com/programmers/lime/domains/member/api/MemberController.java
+++ lime-api/src/main/java/com/programmers/lime/domains/member/api/MemberController.java
@@ -20,26 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.programmers.lime.domains.member.api.dto.request.MemberCheckEmailRequest;
 import com.programmers.lime.domains.member.api.dto.request.MemberCheckNicknameRequest;
-import com.programmers.lime.domains.member.api.dto.request.MemberLoginRequest;
-import com.programmers.lime.domains.member.api.dto.request.MemberSignupRequest;
-import com.programmers.lime.domains.member.api.dto.request.MemberUpdatePasswordRequest;
 import com.programmers.lime.domains.member.api.dto.request.MemberUpdateProfileRequest;
 import com.programmers.lime.domains.member.api.dto.response.AccessTokenResponse;
-import com.programmers.lime.domains.member.api.dto.response.MemberCheckEmailResponse;
 import com.programmers.lime.domains.member.api.dto.response.MemberCheckJwtResponse;
 import com.programmers.lime.domains.member.api.dto.response.MemberCheckNicknameResponse;
 import com.programmers.lime.domains.member.api.dto.response.MemberGetMyPageResponse;
-import com.programmers.lime.domains.member.api.dto.response.MemberLoginResponse;
 import com.programmers.lime.domains.member.application.MemberService;
 import com.programmers.lime.domains.member.application.dto.response.MemberCheckJwtServiceResponse;
-import com.programmers.lime.domains.member.application.dto.response.MemberLoginServiceResponse;
 import com.programmers.lime.domains.member.model.MyPage;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -58,35 +50,6 @@ public class MemberController {
 	public ResponseEntity<MemberCheckJwtResponse> checkJwtToken() {
 		final MemberCheckJwtServiceResponse serviceResponse = memberService.checkJwtToken();
 		final MemberCheckJwtResponse response = MemberCheckJwtResponse.from(serviceResponse);
-
-		return ResponseEntity.ok(response);
-	}
-
-	@Operation(summary = "회원가입", description = "MemberSignupRequest 을 이용하여 회원가입을 합니다.")
-	@PostMapping("/signup")
-	public ResponseEntity<Void> signup(@Valid @RequestBody final MemberSignupRequest request) {
-		memberService.signup(request.toLoginInfo(), request.nickname());
-
-		return ResponseEntity.ok().build();
-	}
-
-	@Operation(summary = "로그인", description = "MemberLoginRequest 을 이용하여 로그인을 합니다.")
-	@PostMapping("/login")
-	public ResponseEntity<MemberLoginResponse> login(
-		HttpServletResponse httpServletResponse,
-		@Valid @RequestBody final MemberLoginRequest request
-	) {
-		final MemberLoginServiceResponse serviceResponse = memberService.login(request.toLoginInfo());
-		final MemberLoginResponse response = MemberLoginResponse.from(serviceResponse);
-
-		final ResponseCookie cookie = ResponseCookie.from("refresh-token", serviceResponse.refreshToken())
-			.maxAge(COOKIE_AGE_SECONDS)
-			.secure(true)
-			.httpOnly(true)
-			.sameSite("None")
-			.path("/")
-			.build();
-		httpServletResponse.addHeader(SET_COOKIE, cookie.toString());
 
 		return ResponseEntity.ok(response);
 	}
@@ -127,14 +90,6 @@ public class MemberController {
 		return ResponseEntity.ok().build();
 	}
 
-	@Operation(summary = "비밀번호 변경", description = "MemberUpdatePasswordRequest 을 이용하여 비밀번호를 변경합니다.")
-	@PutMapping("/password")
-	public ResponseEntity<Void> updatePassword(@Valid @RequestBody final MemberUpdatePasswordRequest request) {
-		memberService.updatePassword(request.password());
-
-		return ResponseEntity.ok().build();
-	}
-
 	@Operation(summary = "닉네임 중복 확인", description = "MemberCheckNicknameRequest 을 이용하여 닉네임 중복 확인을 합니다.")
 	@PostMapping("/check/nickname")
 	public ResponseEntity<MemberCheckNicknameResponse> checkNickname(
@@ -143,17 +98,6 @@ public class MemberController {
 		memberService.checkNickname(request.nickname());
 
 		return ResponseEntity.ok().build();
-	}
-
-	@Operation(summary = "이메일 인증", description = "MemberCheckEmailRequest 을 이용하여 이메일 중복 확인을 한 후 이메일 인증번호를 보냅니다.")
-	@PostMapping("/check/email")
-	public ResponseEntity<MemberCheckEmailResponse> checkEmail(
-		@Valid @RequestBody final MemberCheckEmailRequest request
-	) {
-		final String code = memberService.checkEmail(request.email());
-		final MemberCheckEmailResponse response = new MemberCheckEmailResponse(code);
-
-		return ResponseEntity.ok(response);
 	}
 
 	@Operation(summary = "마이페이지 조회", description = "닉네임을 이용하여 마이페이지를 조회합니다.")
