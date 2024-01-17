cb24922163060546454969d71b91ae4818a89fb1
Curry4182 <kangbk4182@naver.com>
2024-01-17 21:46:34 +0900

    feat: 나의 아이템 폴더 이름 최소, 최대 글자 설정

--- lime-api/src/main/java/com/programmers/lime/domains/item/api/dto/request/MemberItemFolderCreateRequest.java
+++ lime-api/src/main/java/com/programmers/lime/domains/item/api/dto/request/MemberItemFolderCreateRequest.java
@@ -2,6 +2,7 @@ package com.programmers.lime.domains.item.api.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record MemberItemFolderCreateRequest(
 	@Schema(description = "취미", example = "농구")
@@ -10,6 +11,7 @@ public record MemberItemFolderCreateRequest(
 
 	@Schema(description = "폴더 이름", example = "농구 취미 폴더")
 	@NotNull(message = "폴더 이름을 입력하지 않았습니다.")
+	@Size(min = 1, max = 20, message = "폴더 이름은 1자 이상 20자 이하로 입력해주세요.")
 	String folderName
 ){
 }
