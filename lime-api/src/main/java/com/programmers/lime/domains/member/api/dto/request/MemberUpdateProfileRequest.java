0e7e0dfb982151442b3dd68d1386140a988f125e
Yiseul Park <omjl5123@catholic.ac.kr>
2024-01-22 15:11:42 +0900

    fix: 프로필 업데이트, 닉네임 중복확인 request dto 유효성 검증 추가

--- lime-api/src/main/java/com/programmers/lime/domains/member/api/dto/request/MemberUpdateProfileRequest.java
+++ lime-api/src/main/java/com/programmers/lime/domains/member/api/dto/request/MemberUpdateProfileRequest.java
@@ -5,9 +5,13 @@ import com.programmers.lime.domains.member.domain.vo.Introduction;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record MemberUpdateProfileRequest(
 	@Schema(description = "닉네임", example = "킹왕짱오리")
+	@Size(min = 3, max = 25, message = "닉네임은 3글자에서 25글자 사이여야 합니다.")
+	@Pattern(regexp = "^[A-Za-z0-9_가-힣]+$", message = "닉네임은 영어 대소문자, 한글, 숫자 그리고 언더스코어만 허용합니다.")
 	@NotNull(message = "닉네임은 필수 값입니다.")
 	String nickname,
 
@@ -15,12 +19,13 @@ public record MemberUpdateProfileRequest(
 	@NotNull(message = "대표 취미는 필수 값입니다.")
 	String hobby,
 
-	@Min(value = 0, message = "취미 경력은 0보다 작을 수 없습니다.")
 	@Schema(description = "취미 경력", example = "1(년차)")
+	@Min(value = 0, message = "취미 경력은 0보다 작을 수 없습니다.")
 	@NotNull(message = "취미 경력은 필수 값입니다.")
 	int career,
 
 	@Schema(description = "자기소개", example = "안녕하세요!")
+	@Size(max = 300, message = "자기소개는 최대 300자 입니다.")
 	@NotNull(message = "자기소개는 필수 값입니다.")
 	String content,
 
