632131ed8b8c5f4c21f5b5b12a783e6487a588c8
Curry4182 <kangbk4182@naver.com>
2024-01-25 04:51:47 +0900

    feat: 리뷰 이미지 url을 저장하기 위한 review image 도메인 추가

---
+++ lime-domain/src/main/java/com/programmers/lime/domains/review/domain/ReviewImage.java
@@ -0,0 +1,36 @@
+package com.programmers.lime.domains.review.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "review_images")
+public class ReviewImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@JoinColumn(name = "review_id", nullable = false)
+	private Long reviewId;
+
+	@Column(name = "image_url", nullable = false)
+	private String imageUrl;
+
+	public ReviewImage(
+		final Long reviewId,
+		final String imageUrl
+	) {
+		this.reviewId = reviewId;
+		this.imageUrl = imageUrl;
+	}
+}
