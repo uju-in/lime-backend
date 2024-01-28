68af67eafe44255ed1ae176d7e53531001fb4271
Curry4182 <kangbk4182@naver.com>
2024-01-29 07:31:05 +0900

    fixture: 리뷰 목록 조회를 위한 fixture 추가

---
+++ lime-domain/src/testFixtures/java/com/programmers/lime/domains/review/ReviewInfoBuilder.java
@@ -0,0 +1,22 @@
+package com.programmers.lime.domains.review;
+
+import java.util.List;
+
+import com.programmers.lime.domains.review.model.ReviewInfo;
+
+public class ReviewInfoBuilder {
+
+	public static ReviewInfo build(final Long reviewId) {
+		return ReviewInfo.builder()
+			.rate(reviewId.intValue() % 5 + 1)
+			.content("content" + reviewId)
+			.reviewId(reviewId)
+			.build();
+	}
+
+	public static List<ReviewInfo> buildMany(final List<Long> reviewIds) {
+		return reviewIds.stream()
+			.map(ReviewInfoBuilder::build)
+			.toList();
+	}
+}
