42ec12a4d7a12ce3362bed7816ce9c1c7218ce5c
Curry4182 <kangbk4182@naver.com>
2024-02-06 04:04:07 +0900

    feat: 버킷의 url 주소로 버킷의 오브젝트를 삭제할 수 있도록 기능 추가

--- lime-infrastructure/src/main/java/com/programmers/lime/s3/S3Manager.java
+++ lime-infrastructure/src/main/java/com/programmers/lime/s3/S3Manager.java
@@ -3,6 +3,7 @@ package com.programmers.lime.s3;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
@@ -87,6 +88,19 @@ public class S3Manager {
 	public URL getUrl(String directory, String fileName) {
 		String fileNameWithDir = getFileNameWithDir(directory, fileName);
 		return amazonS3.getUrl(bucket, fileNameWithDir);
+	}
+
+	public String extractObjectKeyFromUrl(String urlString) throws MalformedURLException {
+		URL url = new URL(urlString);
+		String path = url.getPath();
+
+		// 객체 키는 URL의 path에서 버킷 이름을 제외한 부분
+		return path.substring(1);
+	}
+
+	public void deleteObjectByUrl(String urlString) throws MalformedURLException {
+		String objectKey = extractObjectKeyFromUrl(urlString);
+		deleteFile(bucket, objectKey);
 	}
 
 	private String getFileNameWithDir(final String directory, final String fileName) {
