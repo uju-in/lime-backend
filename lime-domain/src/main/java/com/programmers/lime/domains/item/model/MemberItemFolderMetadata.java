f1369186fa7435934a118824a60ef79968789f0d
Curry4182 <kangbk4182@naver.com>
2024-02-13 00:58:30 +0900

    feat: 폴더에 포함되어 있는 아이템 개수 반환 기능 추가

--- lime-domain/src/main/java/com/programmers/lime/domains/item/model/MemberItemFolderMetadata.java
+++ lime-domain/src/main/java/com/programmers/lime/domains/item/model/MemberItemFolderMetadata.java
@@ -2,7 +2,11 @@ package com.programmers.lime.domains.item.model;
 
 import java.util.List;
 
+import lombok.Builder;
+
+@Builder
 public record MemberItemFolderMetadata (
-	List<String> imageUrls
+	List<String> imageUrls,
+	int itemCount
 ){
 }
