d3933ad13eeb9e02c2b167163aa8f5c0029a9703
Yiseul Park <omjl5123@catholic.ac.kr>
2024-02-28 16:43:14 +0900

    test: 투표 참여 통합 테스트 추가

--- lime-api/src/test/java/com/programmers/lime/domains/vote/application/VoteServiceTest.java
+++ lime-api/src/test/java/com/programmers/lime/domains/vote/application/VoteServiceTest.java
@@ -3,27 +3,41 @@ package com.programmers.lime.domains.vote.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.programmers.lime.IntegrationTest;
 import com.programmers.lime.common.model.Hobby;
 import com.programmers.lime.domains.item.domain.setup.ItemSetup;
 import com.programmers.lime.domains.vote.application.dto.request.VoteCreateServiceRequest;
+import com.programmers.lime.domains.vote.domain.Vote;
+import com.programmers.lime.domains.vote.domain.Voter;
+import com.programmers.lime.domains.vote.domain.setup.VoteSetUp;
+import com.programmers.lime.domains.vote.domain.setup.VoterSetUp;
 import com.programmers.lime.error.BusinessException;
 import com.programmers.lime.error.EntityNotFoundException;
 import com.programmers.lime.error.ErrorCode;
 import com.programmers.lime.global.util.MemberUtils;
+import com.programmers.lime.redis.vote.VoteRankingInfo;
 import com.programmers.lime.redis.vote.VoteRedisManager;
 
 class VoteServiceTest extends IntegrationTest {
 
 	@Autowired
 	private VoteService voteService;
+
+	@Autowired
+	private VoteSetUp voteSetup;
+
+	@Autowired
+	private VoterSetUp voterSetup;
 
 	@Autowired
 	private ItemSetup itemSetup;
@@ -110,6 +124,100 @@ class VoteServiceTest extends IntegrationTest {
 				.isInstanceOf(EntityNotFoundException.class)
 				.hasFieldOrPropertyWithValue("errorCode", ErrorCode.ITEM_NOT_FOUND);
 
+			then(voteRedisManager).shouldHaveNoInteractions();
+		}
+	}
+
+	@Transactional // 지연 로딩을 위해 필요
+	@Nested
+	class ParticipateVote {
+		@Test
+		@DisplayName("투표에 참여한다.")
+		void participateVoteTest() {
+			// given
+			final Long voteId = 1L;
+			final Long itemId = 1L;
+			final Vote vote = voteSetup.saveOne(voteId, 1L, 2L);
+
+			given(memberUtils.getCurrentMemberId())
+				.willReturn(1L);
+
+			willDoNothing()
+				.given(voteRedisManager)
+				.updateRanking(eq(String.valueOf(vote.getHobby())), eq(true), any(VoteRankingInfo.class));
+
+			// when
+			voteService.participateVote(voteId, itemId);
+
+			// then
+			assertThat(vote.getVoters()).hasSize(1);
+
+			// verify
+			then(voteRedisManager).should(times(1))
+				.updateRanking(eq(String.valueOf(vote.getHobby())), eq(true), any(VoteRankingInfo.class));
+		}
+
+		@Test
+		@DisplayName("투표에 재참여한다.")
+		void reParticipateVoteTest() {
+			// given
+			final Long voteId = 1L;
+			final Long itemId = 2L;
+			final Vote vote = voteSetup.saveOne(voteId, 1L, 2L);
+			final Voter voter = voterSetup.saveOne(vote, 1L, 1L);
+
+			given(memberUtils.getCurrentMemberId())
+				.willReturn(1L);
+
+			// when
+			voteService.participateVote(voteId, itemId);
+
+			// then
+			assertThat(vote.getVoters()).hasSize(1);
+			assertThat(voter.getItemId()).isEqualTo(itemId);
+
+			// verify
+			then(voteRedisManager).shouldHaveNoInteractions();
+		}
+
+		@Test
+		@DisplayName("종료된 투표에 참여하려고 하면 예외를 발생시킨다.")
+		void participateVoteWithEndedVoteTest() {
+			// given
+			final Long voteId = 1L;
+			final Vote vote = voteSetup.saveOne(voteId, 1L, 2L);
+
+			vote.close(LocalDateTime.now());
+
+			given(memberUtils.getCurrentMemberId())
+				.willReturn(1L);
+
+			// when & then
+			assertThatThrownBy(() -> voteService.participateVote(voteId, 1L))
+				.isInstanceOf(BusinessException.class)
+				.hasFieldOrPropertyWithValue("errorCode", ErrorCode.VOTE_CANNOT_PARTICIPATE);
+
+			// verify
+			then(voteRedisManager).shouldHaveNoInteractions();
+		}
+
+		@Test
+		@DisplayName("투표에 없는 아이템에 참여하려고 하면 예외를 발생시킨다.")
+		void participateVoteWithNotExistItemTest() {
+			// given
+			final Long voteId = 1L;
+			final Long itemId = 3L;
+			voteSetup.saveOne(voteId, 1L, 2L);
+
+			given(memberUtils.getCurrentMemberId())
+				.willReturn(1L);
+
+			// when & then
+			assertThatThrownBy(() -> voteService.participateVote(voteId, itemId))
+				.isInstanceOf(BusinessException.class)
+				.hasFieldOrPropertyWithValue("errorCode", ErrorCode.VOTE_NOT_CONTAIN_ITEM);
+
+			// verify
 			then(voteRedisManager).shouldHaveNoInteractions();
 		}
 	}
---
+++ lime-domain/src/testFixtures/java/com/programmers/lime/domains/vote/domain/setup/VoteSetUp.java
@@ -0,0 +1,26 @@
+package com.programmers.lime.domains.vote.domain.setup;
+
+import org.springframework.stereotype.Component;
+
+import com.programmers.lime.domains.vote.domain.Vote;
+import com.programmers.lime.domains.vote.domain.VoteBuilder;
+import com.programmers.lime.domains.vote.repository.VoteRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class VoteSetUp {
+
+	private final VoteRepository voteRepository;
+
+	public Vote saveOne(
+		final Long voteId,
+		final Long item1Id,
+		final Long item2Id
+	) {
+		final Vote vote = VoteBuilder.build(voteId, item1Id, item2Id);
+
+		return voteRepository.save(vote);
+	}
+}
