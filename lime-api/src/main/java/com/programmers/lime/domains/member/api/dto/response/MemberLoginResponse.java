17d4d60991f592936bb310d87498d642d4207f97
JoJaeHyeon <joje1003@naver.com>
2024-03-07 12:34:30 +0900

    refactor : 소셜 id long타입으로 변경

--- lime-api/src/main/java/com/programmers/lime/domains/member/api/dto/response/MemberLoginResponse.java
+++ lime-api/src/main/java/com/programmers/lime/domains/member/api/dto/response/MemberLoginResponse.java
@@ -5,16 +5,19 @@ import com.programmers.lime.domains.member.domain.Member;
 public record MemberLoginResponse(
         Long memberId,
 		String nickname,
-		String accessToken
+		String accessToken,
+		String refreshToken
 ) {
 	public static MemberLoginResponse from(
 		final Member member,
-		final String accessToken
+		final String accessToken,
+		final String refreshToken
 	) {
 		return new MemberLoginResponse(
 			member.getId(),
 			member.getNickname(),
-			accessToken
+			accessToken,
+			refreshToken
 		);
 	}
 }
