3bb611799e224afb99988df8310ffe9be8377fd2
Curry4182 <kangbk4182@naver.com>
2024-03-18 19:03:36 +0900

    feat: 서버에 웹소켓 요청을 할 때 채팅방 id, 멤버 id를 처리하는 이벤트 리스너 및 레디스 메니저 기능 구현

---
+++ lime-infrastructure/src/main/java/com/programmers/lime/redis/chat/ChatSessionRedisManager.java
@@ -0,0 +1,36 @@
+package com.programmers.lime.redis.chat;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.programmers.lime.redis.chat.model.ChatSessionInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ChatSessionRedisManager {
+	private final RedisTemplate<String, Object> redisTemplate;
+	private final static String SESSION_PREFIX = "chatSession:";
+	private final static String MEMBER_ROOM_PREFIX = "memberRoom:";
+
+	public void saveSession(String sessionId, ChatSessionInfo chatSessionInfo) {
+		redisTemplate.opsForValue().set(SESSION_PREFIX + sessionId, chatSessionInfo);
+		redisTemplate.opsForSet().add(
+			MEMBER_ROOM_PREFIX + chatSessionInfo.roomId() + ":" + chatSessionInfo.memberId(),
+			sessionId
+		);
+	}
+
+	public ChatSessionInfo getSessionInfo(String sessionId) {
+		return (ChatSessionInfo) redisTemplate.opsForValue().get(SESSION_PREFIX + sessionId);
+	}
+
+	public Set<String> getSessionIdsByMemberAndRoom(Long memberId, Long roomId) {
+		Set<Object> rawSet = redisTemplate.opsForSet().members(MEMBER_ROOM_PREFIX + roomId + ":" + memberId);
+		return rawSet.stream().map(Object::toString).collect(Collectors.toSet());
+	}
+}
---
+++ lime-infrastructure/src/main/java/com/programmers/lime/redis/chat/model/ChatSessionInfo.java
@@ -0,0 +1,4 @@
+package com.programmers.lime.redis.chat.model;
+
+public record ChatSessionInfo(Long memberId, Long roomId) {
+}
