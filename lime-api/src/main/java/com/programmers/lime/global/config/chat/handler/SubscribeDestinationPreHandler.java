345b55c48a413ceae4dfde4c5c86e6dec3f30abd
Curry4182 <kangbk4182@naver.com>
2024-03-18 18:57:28 +0900

    feat: 구독하고자 하는 destination에 대한 유효성 검사와 권한 검증 하는 interceptor handler 추가

---
+++ lime-api/src/main/java/com/programmers/lime/global/config/chat/handler/SubscribeDestinationPreHandler.java
@@ -0,0 +1,64 @@
+package com.programmers.lime.global.config.chat.handler;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+import com.programmers.lime.error.BusinessException;
+import com.programmers.lime.error.ErrorCode;
+import com.programmers.lime.global.config.chat.subscription.destination.PrivateChatRoomProcessor;
+import com.programmers.lime.global.config.chat.type.DestinationEnum;
+import com.programmers.lime.global.config.chat.type.DestinationType;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SubscribeDestinationPreHandler implements ChannelInterceptor {
+
+	private final PrivateChatRoomProcessor privateChatRoomProcessor;
+
+	/**
+	 * 클라이언트가 구독할 때, 클라이언트의 요청을 처리하기 전에 실행됩니다.
+	 * 구독하고자 하는 destination에 대한 유효성 검사를 수행합니다.
+	 * 추가적으로 destination에 따라 검증 작업을 수행합니다.
+	 */
+	@Override
+	public Message<?> preSend(
+		final Message<?> message,
+		final org.springframework.messaging.MessageChannel channel
+	) {
+		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+		SimpMessageType messageType = accessor.getMessageType();
+
+		if(messageType == SimpMessageType.HEARTBEAT) {
+			return message;
+		}
+
+		StompCommand command = accessor.getCommand();
+		if(command != StompCommand.SUBSCRIBE) {
+			return message;
+		}
+
+		String destination = accessor.getDestination();
+		if(destination == null) {
+			throw new BusinessException(ErrorCode.INVALID_SUBSCRIPTION_DESTINATION);
+		}
+
+		DestinationEnum destinationEnum = DestinationEnum.findByPath(destination);
+
+		if(destinationEnum == null) {
+			throw new BusinessException(ErrorCode.SUBSCRIPTION_DESTINATION_NOT_FOUND);
+		}
+
+		if(destinationEnum.getType() == DestinationType.PRIVATE_CHAT_ROOM) {
+			privateChatRoomProcessor.process(accessor);
+		}
+
+		return message;
+	}
+}
---
+++ lime-api/src/main/java/com/programmers/lime/global/config/chat/type/DestinationType.java
@@ -0,0 +1,17 @@
+package com.programmers.lime.global.config.chat.type;
+
+public enum DestinationType {
+
+	PRIVATE_CHAT_ROOM("PRIVATE_CHAT_ROOM"),
+	PUBLIC_CHAT_ROOM("PUBLIC_CHAT_ROOM");
+
+	private final String description;
+
+	DestinationType(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+}
