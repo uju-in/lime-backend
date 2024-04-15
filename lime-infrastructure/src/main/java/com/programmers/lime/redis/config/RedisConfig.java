1e23f1941044227e6efd0383d9f1be97148e504c
Curry4182 <kangbk4182@naver.com>
2024-04-15 22:56:43 +0900

    feat: redis 클러스터 적용

--- lime-infrastructure/src/main/java/com/programmers/lime/redis/config/RedisConfig.java
+++ lime-infrastructure/src/main/java/com/programmers/lime/redis/config/RedisConfig.java
@@ -1,32 +1,44 @@
 package com.programmers.lime.redis.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
+@ConfigurationProperties("spring.redis.cluster")
 public class RedisConfig {
 
-	@Value("${spring.data.redis.host}")
-	private String host;
-
-	@Value("${spring.data.redis.port}")
-	private int port;
+	@Value("${spring.data.redis.cluster.nodes}")
+	private String clusterNodesString;
 
 	@Value("${spring.data.redis.password}")
 	private String password;
 
+	private List<String> clusterNodes;
+
+	@PostConstruct
+	public void init() {
+		clusterNodes = Arrays.asList(clusterNodesString.split(","));
+	}
+
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
-		configuration.setPassword(password);
-		return new LettuceConnectionFactory(configuration);
+	public LettuceConnectionFactory redisConnectionFactory() {
+		RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration(clusterNodes);
+		clusterConfiguration.setPassword(RedisPassword.of(password));
+
+		return new LettuceConnectionFactory(clusterConfiguration);
 	}
 
 	@Bean
